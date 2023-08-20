abfa154c47b3ece29c1df28ca976f3ca147405eb
janith nirmal <113751270+janithnirmal@users.noreply.github.com>
2023-08-20 13:27:46 +0530

    file structure added

---
+++ backend/model/response_sender.php
@@ -0,0 +1,17 @@
+<?php 
+
+class response_sender {
+
+    public static function sendJson ($responseObject){
+
+        echo(json_encode($responseObject));
+        exit();
+
+
+    }
+
+}
+
+
+?>
+
---
+++ frontend/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ALG005 - savi dessert</title>
+
+    <!-- css -->
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="css/style.css">
+
+    <!-- script -->
+    <script defer src="js/bootstrap.bundle.js"></script>
+    <script defer src="js/script.js"></script>
+</head>
+
+<body>
+
+    <!-- content -->
+
+</body>
+
+</html>
