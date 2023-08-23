7f3e9b8c977011baf1ee9cd10824288d3ce02cf3
janith nirmal <113751270+janithnirmal@users.noreply.github.com>
2023-08-23 06:24:33 +0530

    access control for banned ip address funciotnality aded

--- backend/model/log/error_logger.php
+++ backend/model/log/error_logger.php
@@ -20,6 +20,7 @@ class ErrorLogger
         $logEntry = "[" . $now->format('Y-m-d H:i:s') . "] " . $error . PHP_EOL;
         if (!empty($metadata)) {
             $logEntry .= "Metadata: " . json_encode($metadata) . PHP_EOL . PHP_EOL;
+            $logEntry .= "Info: " . json_encode($this->getUserInfo()) . PHP_EOL . PHP_EOL;
         }
 
         if (!file_exists($logPath)) {
@@ -47,6 +48,38 @@ class ErrorLogger
         }
     }
 
+    private function getUserInfo()
+    {
+        $userInfo = new stdClass();
+
+        // User Agent
+        $userInfo->userAgent = isset($_SERVER['HTTP_USER_AGENT']) ? $_SERVER['HTTP_USER_AGENT'] : '';
+
+        // IP Address
+        $userInfo->ipAddress = isset($_SERVER['REMOTE_ADDR']) ? $_SERVER['REMOTE_ADDR'] : '';
+
+        // Request Headers
+        $userInfo->acceptLanguage = isset($_SERVER['HTTP_ACCEPT_LANGUAGE']) ? $_SERVER['HTTP_ACCEPT_LANGUAGE'] : '';
+        $userInfo->referer = isset($_SERVER['HTTP_REFERER']) ? $_SERVER['HTTP_REFERER'] : '';
+
+        // Session Information
+        session_start(); // Start the session (if not started already)
+        $userInfo->sessionId = session_id();
+
+        // Cookies
+        $userInfo->cookieValue = isset($_COOKIE['cookie_name']) ? $_COOKIE['cookie_name'] : '';
+
+        // Query Parameters
+        $userInfo->paramValue = isset($_GET['param_name']) ? $_GET['param_name'] : '';
+
+        // POST Data
+        $userInfo->postData = $_POST; // This includes all POST data
+
+        return $userInfo;
+    }
+
+
+    // finalized one for long time period
     // private function deleteOldFiles($directory, $daysThreshold) {
     //     $thresholdDate = new DateTime("-$daysThreshold days");
     //     $directoryContents = scandir($directory);
