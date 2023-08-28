7806ba54b36a0d4dc6d96734091751875e041182
MADUSHADEV <madusha.dev001@gmail.com>
2023-08-28 10:24:54 +0530

    new database upload
    upload new database ALG005 2023/08/28

--- backend/database/backups/ALG005_2023_08_28.sql
+++ backend/database/backups/ALG005_2023_08_28.sql
@@ -16,6 +16,41 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Table structure for table `card`
+--
+
+DROP TABLE IF EXISTS `card`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `card` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `qty` int NOT NULL,
+  `product_item_id` int NOT NULL,
+  `extra_item_id` int NOT NULL,
+  `user_user_id` int NOT NULL,
+  `weight_id` int NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_card_user1_idx` (`user_user_id`),
+  KEY `fk_card_product_item1_idx` (`product_item_id`),
+  KEY `fk_card_extra_item1_idx` (`extra_item_id`),
+  KEY `fk_card_weight1_idx` (`weight_id`),
+  CONSTRAINT `fk_card_extra_item1` FOREIGN KEY (`extra_item_id`) REFERENCES `extra_item` (`id`),
+  CONSTRAINT `fk_card_product_item1` FOREIGN KEY (`product_item_id`) REFERENCES `product_item` (`id`),
+  CONSTRAINT `fk_card_user1` FOREIGN KEY (`user_user_id`) REFERENCES `user` (`user_id`),
+  CONSTRAINT `fk_card_weight1` FOREIGN KEY (`weight_id`) REFERENCES `weight` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `card`
+--
+
+LOCK TABLES `card` WRITE;
+/*!40000 ALTER TABLE `card` DISABLE KEYS */;
+/*!40000 ALTER TABLE `card` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `category`
 --
 
@@ -107,7 +142,7 @@ CREATE TABLE `extra` (
   PRIMARY KEY (`id`),
   KEY `fk_extra_extra_status1_idx` (`extra_status_id`),
   CONSTRAINT `fk_extra_extra_status1` FOREIGN KEY (`extra_status_id`) REFERENCES `extra_status` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb3;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb3;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -116,7 +151,7 @@ CREATE TABLE `extra` (
 
 LOCK TABLES `extra` WRITE;
 /*!40000 ALTER TABLE `extra` DISABLE KEYS */;
-INSERT INTO `extra` VALUES (1,1,'grapes'),(2,1,'Cashew Nuts'),(3,1,'Banana');
+INSERT INTO `extra` VALUES (1,1,'grapes'),(2,1,'Cashew Nuts'),(3,1,'Banana'),(4,1,'No Item');
 /*!40000 ALTER TABLE `extra` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -132,8 +167,8 @@ CREATE TABLE `extra_item` (
   `extra_id` int NOT NULL,
   `product_product_id` varchar(16) NOT NULL,
   PRIMARY KEY (`id`),
-  KEY `fk_extra_item_extra1_idx` (`extra_id`),
   KEY `fk_extra_item_product1_idx` (`product_product_id`),
+  KEY `fk_extra_item_extra1_idx` (`extra_id`),
   CONSTRAINT `fk_extra_item_extra1` FOREIGN KEY (`extra_id`) REFERENCES `extra` (`id`),
   CONSTRAINT `fk_extra_item_product1` FOREIGN KEY (`product_product_id`) REFERENCES `product` (`product_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb3;
@@ -145,7 +180,7 @@ CREATE TABLE `extra_item` (
 
 LOCK TABLES `extra_item` WRITE;
 /*!40000 ALTER TABLE `extra_item` DISABLE KEYS */;
-INSERT INTO `extra_item` VALUES (1,3,'987662514'),(2,1,'123456789');
+INSERT INTO `extra_item` VALUES (1,0,'987662514'),(2,0,'123456789');
 /*!40000 ALTER TABLE `extra_item` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -213,13 +248,13 @@ CREATE TABLE `product_item` (
   `id` int NOT NULL AUTO_INCREMENT,
   `qty` int NOT NULL,
   `price` double NOT NULL,
-  `weight_id` int NOT NULL,
   `product_status_id` int NOT NULL,
   `product_product_id` varchar(16) NOT NULL,
+  `weight_id` int NOT NULL,
   PRIMARY KEY (`id`),
-  KEY `fk_product_item_weight1_idx` (`weight_id`),
   KEY `fk_product_item_product_status1_idx` (`product_status_id`),
   KEY `fk_product_item_product1_idx` (`product_product_id`),
+  KEY `fk_product_item_weight1_idx` (`weight_id`),
   CONSTRAINT `fk_product_item_product1` FOREIGN KEY (`product_product_id`) REFERENCES `product` (`product_id`),
   CONSTRAINT `fk_product_item_product_status1` FOREIGN KEY (`product_status_id`) REFERENCES `product_status` (`id`),
   CONSTRAINT `fk_product_item_weight1` FOREIGN KEY (`weight_id`) REFERENCES `weight` (`id`)
@@ -232,7 +267,7 @@ CREATE TABLE `product_item` (
 
 LOCK TABLES `product_item` WRITE;
 /*!40000 ALTER TABLE `product_item` DISABLE KEYS */;
-INSERT INTO `product_item` VALUES (1,10,1000,1,1,'122314522'),(2,2,90,2,1,'122314522'),(3,20,1500,1,1,'123456789'),(4,12,3000,3,1,'987662514'),(5,1,4000,3,1,'122314522');
+INSERT INTO `product_item` VALUES (1,10,1000,1,'122314522',0),(2,2,90,1,'122314522',0),(3,20,1500,1,'123456789',0),(4,12,3000,1,'987662514',0),(5,1,4000,1,'122314522',0);
 /*!40000 ALTER TABLE `product_item` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -245,7 +280,7 @@ DROP TABLE IF EXISTS `product_status`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `product_status` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `status` varchar(12) NOT NULL,
+  `type` varchar(12) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb3;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -256,8 +291,63 @@ CREATE TABLE `product_status` (
 
 LOCK TABLES `product_status` WRITE;
 /*!40000 ALTER TABLE `product_status` DISABLE KEYS */;
-INSERT INTO `product_status` VALUES (1,'In Stock'),(2,'No Stock');
+INSERT INTO `product_status` VALUES (1,''),(2,'');
 /*!40000 ALTER TABLE `product_status` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `promotion`
+--
+
+DROP TABLE IF EXISTS `promotion`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `promotion` (
+  `promotion_id` int NOT NULL AUTO_INCREMENT,
+  `start_date_time` datetime NOT NULL,
+  `end_date_time` datetime NOT NULL,
+  `product_item_id` int NOT NULL,
+  `promotion_status_promotion_status_id` int NOT NULL,
+  PRIMARY KEY (`promotion_id`),
+  KEY `fk_promotion_product_item1_idx` (`product_item_id`),
+  KEY `fk_promotion_promotion_status1_idx` (`promotion_status_promotion_status_id`),
+  CONSTRAINT `fk_promotion_product_item1` FOREIGN KEY (`product_item_id`) REFERENCES `product_item` (`id`),
+  CONSTRAINT `fk_promotion_promotion_status1` FOREIGN KEY (`promotion_status_promotion_status_id`) REFERENCES `promotion_status` (`promotion_status_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `promotion`
+--
+
+LOCK TABLES `promotion` WRITE;
+/*!40000 ALTER TABLE `promotion` DISABLE KEYS */;
+INSERT INTO `promotion` VALUES (1,'2023-08-28 09:35:18','2023-08-28 09:35:20',1,1);
+/*!40000 ALTER TABLE `promotion` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `promotion_status`
+--
+
+DROP TABLE IF EXISTS `promotion_status`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `promotion_status` (
+  `promotion_status_id` int NOT NULL AUTO_INCREMENT,
+  `status` varchar(10) NOT NULL,
+  PRIMARY KEY (`promotion_status_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `promotion_status`
+--
+
+LOCK TABLES `promotion_status` WRITE;
+/*!40000 ALTER TABLE `promotion_status` DISABLE KEYS */;
+INSERT INTO `promotion_status` VALUES (1,'active'),(2,'deactive');
+/*!40000 ALTER TABLE `promotion_status` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -271,15 +361,15 @@ CREATE TABLE `user` (
   `user_id` int NOT NULL AUTO_INCREMENT,
   `email` varchar(50) NOT NULL,
   `full_name` varchar(50) NOT NULL,
-  `password_salt` varchar(64) NOT NULL,
-  `password_hash` varchar(64) NOT NULL,
+  `password_salt` varchar(240) NOT NULL,
+  `password_hash` varchar(240) NOT NULL,
   `confomation_code` int NOT NULL,
   `status_id` int NOT NULL,
   `register_date` date NOT NULL,
   PRIMARY KEY (`user_id`),
   KEY `fk_user_status1_idx` (`status_id`),
   CONSTRAINT `fk_user_status1` FOREIGN KEY (`status_id`) REFERENCES `user_status` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb3;
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb3;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -288,7 +378,7 @@ CREATE TABLE `user` (
 
 LOCK TABLES `user` WRITE;
 /*!40000 ALTER TABLE `user` DISABLE KEYS */;
-INSERT INTO `user` VALUES (1,'madusha@gmail.com','Madusha Pravinda','111112222','111112222',0,1,'2023-08-22');
+INSERT INTO `user` VALUES (1,'madusha@gmail.com','Madusha Pravinda','111112222','111112222',0,1,'2023-08-22'),(2,'armadushapravinda@gmail.com','maduwa','8e55a61497b791ff6e22392f79fb0386','7788caaba88ca399937589c8f2b060f0cbf6a7654ae3c659f27c03f01637bf5c4eb06da55acaaafefe362d3b81c8d0238b2b26b9fd13c44d5851391584dea55d',0,1,'2023-08-23'),(3,'kamalmadushapravinda@gmail.com','maduwa','aaeeb73c5fa27e8db8302cabb5854f84','1a000d41ece16bda6ac1000a5aa981f729e3be1151e65bfc19fb4876627a54962dbe5f5f380e5d3bb6552df3481d79900d24af98347fefb6145ba45899fdaa6d',0,1,'2023-08-23'),(4,'nimalmadushapravinda@gmail.com','maduwa','0d14670b47112767adbf180c93ac6bee','3a4347f65db6b934a0a04f4b8c933a88863685035c507a52cc82961ed4c7b1c53159d5b65a5a8e8570f702a3100ab244201b899fa8e74c2fb5a03997011e4637',0,1,'2023-08-23'),(5,'sirilmadushapravinda@gmail.com','maduwa','6898e6df9303e667b1ecaeb37c9b6ccf','46ae7aafdf5aa844113ffbbacc2187dd261921f5bb4ff56b8e06886f088ef9d9b514d289b2fb31535223ca97c8ea793960c3e7135c52dcf7f9a9247ce9d91651',0,1,'2023-08-23'),(6,'marusira@gmail.com','maduwa','11267714ab2c9cc2cc996e4c8aa26338','f7f4c947a808244921d0d9bd0a5c0616ceff6a08924f1b72b646fc40cf5cc6d39588d082774001f4ab572b382cd4f45f97d843f282067375b39bd2a1a76748ed',0,1,'2023-08-23'),(7,'saranadarmakirthi@gmail.com','maduwa','a74efe9739197e89dca052abd2886f54','af37d687b4c6446062e83aaa9d4732e1da8330d45b174f3fbfa84479e814ec7dd283c4974c346eb479aefe1d37a17f1afb4a03afcaf15cb9213ed6eb2a5063df',0,1,'2023-08-23'),(8,'supundarmakirthi@gmail.com','Supun Silva','7dc46b32d4dfe718079b11a845f35e68','6e1aeccbdd3795d4ae438ff1ce1b37cf089b8431f5b6b4450fffd52ee1643152621e985e3d72419e40ad2bc579e8f702a430db8dc99898172d7a11e9aaa42e90',0,1,'2023-08-23'),(9,'palakore@gmail.com','Supun Silva','cb91381c9a6170adace37079011db19d','387cae1230294438fe68b7880fde54159546539a72407aa94224a6f5a9dbe5f84aef939cda36b18deca02b6a2255762d4f2ba596bda95e8fed8381a77ddcbf30',0,1,'2023-08-23');
 /*!40000 ALTER TABLE `user` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -314,6 +404,34 @@ LOCK TABLES `user_status` WRITE;
 /*!40000 ALTER TABLE `user_status` DISABLE KEYS */;
 INSERT INTO `user_status` VALUES (1,'active'),(2,'no active'),(3,'block');
 /*!40000 ALTER TABLE `user_status` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `watchlist`
+--
+
+DROP TABLE IF EXISTS `watchlist`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `watchlist` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `product_item_id` int NOT NULL,
+  `user_user_id` int NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_watchlist_user1_idx` (`user_user_id`),
+  KEY `fk_watchlist_product_item1_idx` (`product_item_id`),
+  CONSTRAINT `fk_watchlist_product_item1` FOREIGN KEY (`product_item_id`) REFERENCES `product_item` (`id`),
+  CONSTRAINT `fk_watchlist_user1` FOREIGN KEY (`user_user_id`) REFERENCES `user` (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `watchlist`
+--
+
+LOCK TABLES `watchlist` WRITE;
+/*!40000 ALTER TABLE `watchlist` DISABLE KEYS */;
+/*!40000 ALTER TABLE `watchlist` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -349,4 +467,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-08-22 22:50:40
+-- Dump completed on 2023-08-28 10:22:42
