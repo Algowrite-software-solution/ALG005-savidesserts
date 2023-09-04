4ac6e430bba1750849d0eb5790f1e98015bba722
NisalUthsara <117739668+NisalUthsara@users.noreply.github.com>
2023-09-04 16:11:39 +0530

    09/4/2023

--- frontend/pages/components/header.php
+++ frontend/pages/components/header.php
@@ -1,6 +1,6 @@
 <header>
     <nav>
-        <div class="row m-auto alg-bg-dark position-relative px-lg-2">
+        <div class="row m-auto alg-bg-dark position-relative px-lg-5 px-3">
             <div class="col-12 d-flex justify-content-between align-items-center m-0 p-0">
 
                 <div class="p-3">
@@ -9,20 +9,26 @@
 
                 <div class="d-none d-md-block d-lg-block">
                     <div class=" d-flex gap-4 cursor ">
-                        <div class="alg-bg-tan px-5 py-1 rounded-4 position-absolute fw-bold alg-button-hover"><span><a href="index.php" class="text-decoration-none text-black">Home</a></span></div>
-                        <div class="alg-bg-light px-5 py-1 rounded-4 alg-navmarginLeft fw-bold alg-button-hover"><span><a href="cardItem.php" class="text-decoration-none text-black">Products</a></span></div>
+                        <div class="alg-bg-tan px-5 py-1 rounded-4 position-absolute fw-bold alg-button-hover"><span><a
+                                    href="index.php" class="text-decoration-none text-black">Home</a></span></div>
+                        <div class="alg-bg-light px-5 py-1 rounded-4 alg-navmarginLeft fw-bold alg-button-hover">
+                            <span><a href="cardItem.php" class="text-decoration-none text-black">Products</a></span>
+                        </div>
                     </div>
                 </div>
 
                 <div class="d-flex gap-3 align-items-center">
                     <div class="d-none d-md-block d-lg-block mx-2 ">
-                        <a href="" class="alg-button-hover" data-bs-toggle="modal" data-bs-target="#staticBackdrop1"><i class="bi bi-cart-fill alg-header-text fs-4 mx-3 alg-text-hover"></i></a>
-                        <a href="" data-bs-toggle="modal" data-bs-target="#staticBackdrop2"><i class="bi bi-heart-fill alg-header-text fs-4 alg-text-hover"></i></a>
+                        <a href="" class="alg-button-hover" data-bs-toggle="modal" data-bs-target="#staticBackdrop1"><i
+                                class="bi bi-cart-fill alg-header-text fs-4 mx-3 alg-text-hover"></i></a>
+                        <a href="" data-bs-toggle="modal" data-bs-target="#staticBackdrop2"><i
+                                class="bi bi-heart-fill alg-header-text fs-4 alg-text-hover"></i></a>
                     </div>
                     <div class="d-flex align-items-center gap-3">
                         <div class="d-flex align-items-center gap-3 cursor">
                             <span class="rounded-circle"><i class="bi bi-whatsapp text-success fs-4"></i></i></span>
-                            <div class="alg-user-Word alg-bg-gold"><span class="d-flex justify-content-center align-items-center fw-semibold">ml</span></div>
+                            <div class="alg-user-Word alg-bg-gold"><span
+                                    class="d-flex justify-content-center align-items-center fw-semibold">ml</span></div>
                         </div>
                         <div class="alg-toggle-button d-md-none d-lg-none fs-2">
                             <i class="bx bx-menu alg-header-text alg-nav-hover"></i>
@@ -34,10 +40,14 @@
             </div>
             <div class="text-center nav_box alg-bg-gold bg-opacity-50 position-static">
                 <div class="d-flex flex-column d-block d-md-none pb-3">
-                    <span class="mt-3 alg-div-hover"><a href="index.php" class="text-decoration-none fw-semibold">Home</a></span>
-                    <span class="mt-3 alg-div-hover"><a href="cardItem.php" class="text-decoration-none fw-semibold">Products</a></span>
-                    <span class="mt-3 alg-div-hover" data-bs-toggle="modal" data-bs-target="#staticBackdrop1"><a href="#cart" class="text-decoration-none fw-semibold">Cart</a></span>
-                    <span class="mt-3 alg-div-hover" data-bs-toggle="modal" data-bs-target="#staticBackdrop2"><a href="#watchlist" class="text-decoration-none fw-semibold">Watchlist</a></span>
+                    <span class="mt-3 alg-div-hover"><a href="index.php"
+                            class="text-decoration-none fw-semibold">Home</a></span>
+                    <span class="mt-3 alg-div-hover"><a href="cardItem.php"
+                            class="text-decoration-none fw-semibold">Products</a></span>
+                    <span class="mt-3 alg-div-hover" data-bs-toggle="modal" data-bs-target="#staticBackdrop1"><a
+                            href="#cart" class="text-decoration-none fw-semibold">Cart</a></span>
+                    <span class="mt-3 alg-div-hover" data-bs-toggle="modal" data-bs-target="#staticBackdrop2"><a
+                            href="#watchlist" class="text-decoration-none fw-semibold">Watchlist</a></span>
                 </div>
             </div>
         </div>
--- frontend/pages/components/testamonials.php
+++ frontend/pages/components/testamonials.php
@@ -26,8 +26,9 @@
 <!-- Start testamonials -->
 
 <section>
-    <div class="col-12 d-flex flex-column justify-content-center align-items-center testm-main w-100 pt-3 alg-bg-dark px-5">
-        <span class="alg-text-h2 alg-text-dark fw-bold">TESTAMONIALS</span>
+    <div
+        class="col-12 d-flex flex-column justify-content-center align-items-center testm-main w-100 pt-3 alg-bg-dark px-5">
+        <span class="alg-text-h2 text-white fw-bold">TESTAMONIALS</span>
         <div class="col-10 d-flex flex-lg-row flex-column justify-content-center align-items-center m-4 ">
             <div class="testm-card d-flex justify-content-center align-items-center m-3 alg-bg-light">
                 <div class="testm-card-img m-3"></div>
@@ -45,10 +46,10 @@
                     </h5>
                 </div>
             </div>
-            <div class="testm-card d-flex justify-content-center align-items-center m-3">
+            <div class="testm-card d-flex justify-content-center align-items-center m-3 alg-bg-light">
                 <div class="testm-card-img m-3"></div>
                 <div class="px-3 pt-3">
-                    <p class="text-white card-para">"Pure nostalgia in every bite!
+                    <p class=" card-para alg-text-dark fw-bold">"Pure nostalgia in every bite!
                         [Your Dessert Brand Name] captures
                         the essence of Sri Lankan sweets
                         perfectly. Each dessert is a journey
@@ -56,15 +57,15 @@
                         warmth and flavour. Absolutely love
                         it!"
                     </p>
-                    <h5 class="text-white">
+                    <h5 class="alg-text-gold">
                         - Rajeev D.
                     </h5>
                 </div>
             </div>
-            <div class="testm-card d-flex justify-content-center align-items-center m-3">
+            <div class="testm-card d-flex justify-content-center align-items-center m-3 alg-bg-light">
                 <div class="testm-card-img m-3"></div>
                 <div class="px-3 pt-3">
-                    <p class="text-white card-para">"Pure nostalgia in every bite!
+                    <p class=" card-para alg-text-dark fw-bold">"Pure nostalgia in every bite!
                         [Your Dessert Brand Name] captures
                         the essence of Sri Lankan sweets
                         perfectly. Each dessert is a journey
@@ -72,7 +73,7 @@
                         warmth and flavour. Absolutely love
                         it!"
                     </p>
-                    <h5 class="text-white">
+                    <h5 class="alg-text-gold">
                         - Rajeev D.
                     </h5>
                 </div>
