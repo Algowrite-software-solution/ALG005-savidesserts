966c89a2c547d17d6ba936fdf1564cd3ca2a7235
NisalUthsara <117739668+NisalUthsara@users.noreply.github.com>
2023-09-07 16:10:36 +0530

    SignIn modal ui update

--- frontend/pages/components/signInModel.php
+++ frontend/pages/components/signInModel.php
@@ -65,17 +65,19 @@
 
                             </div>
 
-                            <div class="form-outline mb-4">
-                                <input type="password" id="form2Example2" class="form-control rounded-5"
-                                    placeholder="Retype the Password" />
-
-                            </div>
-
                             <!-- Submit button -->
                             <div class="d-flex justify-content-center align-items-center ">
                                 <button type="submit"
                                     class="p-2 mb-4 w-100 rounded-5 ALG-model-button text-white fw-bolder">Sign
                                     in</button>
+                            </div>
+
+                            <!-- Register buttons -->
+                            <div class="text-center">
+                                <button type="button" class="btn text-primary" data-bs-toggle="modal"
+                                    data-bs-target="#ALG-forgotPassword-Modal">
+                                    <p>Forgot your password?</p>
+                                </button>
                             </div>
 
                             <!-- Register buttons -->
@@ -140,6 +142,12 @@
                             </div>
 
 
+                            <div class="form-outline mb-4">
+                                <input type="password" id="signUp-retypepassword" class="form-control rounded-5"
+                                    placeholder="Retype the Password" />
+
+                            </div>
+
                             <!-- Submit button -->
                             <div class="d-flex justify-content-center align-items-center ">
                                 <button type="submit"
@@ -156,6 +164,47 @@
     </div>
 
     <!-- SignUp modal -->
+
+    <!--forgotPassword Modal -->
+    <div class="modal fade" id="ALG-forgotPassword-Modal" tabindex="-1" aria-labelledby="ALG-forgotPassword-Modal-Label"
+        aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body ALG-main-model">
+                    <div class=" d-flex justify-content-center align-items-center flex-column pb-3">
+                        <h3 class="text-white">Forgot Your password?</h3>
+                    </div>
+                    <div class="ALG-main-model2 p-3 rounded-5">
+
+                        <p class="m-2 mb-4">If you have forgotten your password, you can reset it here.</p>
+
+                        <form>
+                            <!-- Email input -->
+                            <div class="form-outline mb-4">
+                                <input type="email" id="#" class="ALG-model-input form-control rounded-5"
+                                    placeholder="Email address" />
+
+                            </div>
+
+                            <!-- Submit button -->
+                            <div class="d-flex justify-content-center align-items-center ">
+                                <button type="submit"
+                                    class="p-2 mb-4 w-100 rounded-5 ALG-model-button text-white fw-bolder">Reset
+                                    Password</button>
+                            </div>
+
+                        </form>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!--forgotPassword Modal -->
+    </div>
 
 </body>
 
