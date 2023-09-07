a2d8edafee57fb0db242df01c23e16dde2b727d1
NisalUthsara <117739668+NisalUthsara@users.noreply.github.com>
2023-09-07 15:08:49 +0530

    testamonials Ui update

--- frontend/pages/components/testamonials.php
+++ frontend/pages/components/testamonials.php
@@ -29,9 +29,11 @@
     <div
         class="col-12 d-flex flex-column justify-content-center align-items-center testm-main w-100 pt-3 alg-bg-dark px-5">
         <span class="alg-text-h2 text-white fw-bold">TESTAMONIALS</span>
-        <div class="col-10 d-flex flex-lg-row flex-column justify-content-center align-items-center m-4 ">
+        <div class="col-12 d-flex flex-lg-row flex-column justify-content-center align-items-center m-4 ">
             <div class="testm-card d-flex justify-content-center align-items-center m-3 alg-bg-light">
-                <div class="testm-card-img m-3"></div>
+                <div class="p-2"><img src="https://javainstitute.edu.lk/img/faculty/dr-ishantha.jpg"
+                        class="img-fluid testm-card-img ">
+                </div>
                 <div class="px-3 pt-3">
                     <p class="card-para alg-text-dark fw-bold">"Pure nostalgia in every bite!
                         [Your Dessert Brand Name] captures
@@ -47,9 +49,11 @@
                 </div>
             </div>
             <div class="testm-card d-flex justify-content-center align-items-center m-3 alg-bg-light">
-                <div class="testm-card-img m-3"></div>
+                <div class="p-2"><img src="https://javainstitute.edu.lk/img/faculty/dr-ishantha.jpg"
+                        class="img-fluid testm-card-img ">
+                </div>
                 <div class="px-3 pt-3">
-                    <p class=" card-para alg-text-dark fw-bold">"Pure nostalgia in every bite!
+                    <p class="card-para alg-text-dark fw-bold">"Pure nostalgia in every bite!
                         [Your Dessert Brand Name] captures
                         the essence of Sri Lankan sweets
                         perfectly. Each dessert is a journey
@@ -63,9 +67,11 @@
                 </div>
             </div>
             <div class="testm-card d-flex justify-content-center align-items-center m-3 alg-bg-light">
-                <div class="testm-card-img m-3"></div>
+                <div class="p-2"><img src="https://javainstitute.edu.lk/img/faculty/dr-ishantha.jpg"
+                        class="img-fluid testm-card-img ">
+                </div>
                 <div class="px-3 pt-3">
-                    <p class=" card-para alg-text-dark fw-bold">"Pure nostalgia in every bite!
+                    <p class="card-para alg-text-dark fw-bold">"Pure nostalgia in every bite!
                         [Your Dessert Brand Name] captures
                         the essence of Sri Lankan sweets
                         perfectly. Each dessert is a journey
