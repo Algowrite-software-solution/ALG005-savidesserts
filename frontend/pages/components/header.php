1cd79f0bbc88af1ee246bf60b55c38f36c728e48
kavindu202 <kavindu.sasanka962@gmail.com>
2023-09-09 00:05:31 +0530

    header class updates

--- frontend/pages/components/header.php
+++ frontend/pages/components/header.php
@@ -9,9 +9,9 @@
                     </div>
 
                     <div class="d-none d-md-block d-lg-block">
-                        <div class=" d-flex gap-4 cursor ">
+                        <div class="d-flex gap-4 alg-cursor">
                             <div class="alg-bg-tan px-5 py-1 rounded-4 position-absolute fw-bold alg-button-hover"><span><a href="index.php" class="text-decoration-none text-black">Home</a></span></div>
-                            <div class="alg-bg-light px-5 py-1 rounded-4 alg-navmarginLeft fw-bold alg-button-hover">
+                            <div class="alg-bg-light px-5 py-1 rounded-4 hd-marginLeft fw-bold alg-button-hover">
                                 <span><a href="cardItem.php" class="text-decoration-none text-black">Products</a></span>
                             </div>
                         </div>
@@ -19,16 +19,16 @@
 
                     <div class="d-flex gap-3 align-items-center">
                         <div class="d-none d-md-block d-lg-block mx-2 ">
-                            <a href="#cart" class="alg-button-hover" data-bs-toggle="modal" data-bs-target="#staticBackdrop1"><i class="bi bi-cart-fill alg-header-text fs-4 mx-3 alg-text-hover"></i></a>
-                            <a href="#watchlist" data-bs-toggle="modal" data-bs-target="#staticBackdrop2"><i class="bi bi-heart-fill alg-header-text fs-4 alg-text-hover"></i></a>
+                            <a href="#cart" class="alg-button-hover" data-bs-toggle="modal" data-bs-target="#staticBackdrop1"><i class="bi bi-cart-fill alg-text-gold fs-4 mx-3 alg-text-hover"></i></a>
+                            <a href="#watchlist" data-bs-toggle="modal" data-bs-target="#staticBackdrop2"><i class="bi bi-heart-fill alg-text-gold fs-4 alg-text-hover"></i></a>
                         </div>
                         <div class="d-flex align-items-center gap-3">
-                            <div class="d-flex align-items-center gap-3 cursor">
+                            <div class="d-flex align-items-center gap-3 alg-cursor">
                                 <span class="rounded-circle"><i class="bi bi-whatsapp text-success fs-4"></i></i></span>
                                 <div class="alg-user-Word alg-bg-gold"><span class="d-flex justify-content-center align-items-center fw-semibold"><a href="profileViewCard.php" class="text-decoration-none text-black">ml</a></span></div>
                             </div>
                             <div class="alg-toggle-button d-md-none d-lg-none fs-2">
-                                <i class="bx bx-menu alg-header-text alg-nav-hover"></i>
+                                <i class="bx bx-menu alg-text-gold alg-nav-hover"></i>
                             </div>
                         </div>
                     </div>
