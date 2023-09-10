e15532a7e2d7b12a28f7119a9725672710663d06
janith nirmal <113751270+janithnirmal@users.noreply.github.com>
2023-09-10 18:13:34 +0530

    ui category updated

--- pages/components/category.php
+++ pages/components/category.php
@@ -6,7 +6,7 @@
                 <div class=" text-center pb-4 pt-3 px-3">
                     <span class="alg-text-h2 alg-text-dark fw-bold">CATEGORY</span>
                     <div class="promotionSwiper swiper mySwiperCategory mt-1">
-                        <div class="swiper-wrapper mx-auto" id="categorySliderContainer">
+                        <div class="swiper-wrapper" id="categorySliderContainer">
                             <!-- category data goes here -->
                         </div>
                         <div class="swiper-button-next alg-text-gold"></div>
--- pages/components/header.php
+++ pages/components/header.php
@@ -14,26 +14,20 @@
                                 <span><a href="index.php" class="text-decoration-none text-black">Home</a></span>
                             </div>
                             <div class="alg-bg-light px-5 py-1 rounded-4 hd-marginLeft fw-bold alg-button-hover">
-                                <span><a href="cardItem.php" class="text-decoration-none text-black">Products</a></span>
+                                <span><a href="products.php" class="text-decoration-none text-black">Products</a></span>
                             </div>
                         </div>
                     </div>
 
                     <div class="d-flex gap-3 align-items-center">
                         <div class="d-none d-md-block d-lg-block mx-2 ">
-                            <a href="#cart" class="alg-button-hover" data-bs-toggle="modal"
-                                data-bs-target="#staticBackdrop1"><i
-                                    class="bi bi-cart-fill alg-text-gold fs-4 mx-3 alg-text-hover"></i></a>
-                            <a href="#watchlist" data-bs-toggle="modal" data-bs-target="#staticBackdrop2"><i
-                                    class="bi bi-heart-fill alg-text-gold fs-4 alg-text-hover"></i></a>
+                            <a href="#cart" class="alg-button-hover" data-bs-toggle="modal" data-bs-target="#staticBackdrop1"><i class="bi bi-cart-fill alg-text-gold fs-4 mx-3 alg-text-hover"></i></a>
+                            <a href="#watchlist" data-bs-toggle="modal" data-bs-target="#staticBackdrop2"><i class="bi bi-heart-fill alg-text-gold fs-4 alg-text-hover"></i></a>
                         </div>
                         <div class="d-flex align-items-center gap-3">
                             <div class="d-flex align-items-center gap-3 alg-cursor">
                                 <span class="rounded-circle"><i class="bi bi-whatsapp text-success fs-4"></i></i></span>
-                                <div class="alg-user-Word alg-bg-gold"><span
-                                        class="d-flex justify-content-center align-items-center fw-semibold"><a
-                                            href="profileViewCard.php"
-                                            class="text-decoration-none text-black">ml</a></span></div>
+                                <div class="alg-user-Word alg-bg-gold"><span class="d-flex justify-content-center align-items-center fw-semibold"><a href="profileViewCard.php" class="text-decoration-none text-black">ml</a></span></div>
                             </div>
                             <div class="alg-toggle-button d-md-none d-lg-none fs-2">
                                 <i class="bx bx-menu alg-text-gold alg-nav-hover"></i>
@@ -44,14 +38,10 @@
                 </div>
                 <div class=" text-center nav-box alg-bg-gold bg-opacity-50 position-static">
                     <div class="d-flex flex-column d-block d-md-none pb-3">
-                        <span class="mt-3 alg-div-hover"><a href="index.php"
-                                class="text-decoration-none fw-semibold">Home</a></span>
-                        <span class="mt-3 alg-div-hover"><a href="cardItem.php"
-                                class="text-decoration-none fw-semibold">Products</a></span>
-                        <span class="mt-3 alg-div-hover" data-bs-toggle="modal" data-bs-target="#staticBackdrop1"><a
-                                href="#cart" class="text-decoration-none fw-semibold">Cart</a></span>
-                        <span class="mt-3 alg-div-hover" data-bs-toggle="modal" data-bs-target="#staticBackdrop2"><a
-                                href="#watchlist" class="text-decoration-none fw-semibold">Watchlist</a></span>
+                        <span class="mt-3 alg-div-hover"><a href="index.php" class="text-decoration-none fw-semibold">Home</a></span>
+                        <span class="mt-3 alg-div-hover"><a href="cardItem.php" class="text-decoration-none fw-semibold">Products</a></span>
+                        <span class="mt-3 alg-div-hover" data-bs-toggle="modal" data-bs-target="#staticBackdrop1"><a href="#cart" class="text-decoration-none fw-semibold">Cart</a></span>
+                        <span class="mt-3 alg-div-hover" data-bs-toggle="modal" data-bs-target="#staticBackdrop2"><a href="#watchlist" class="text-decoration-none fw-semibold">Watchlist</a></span>
                     </div>
                 </div>
             </div>
