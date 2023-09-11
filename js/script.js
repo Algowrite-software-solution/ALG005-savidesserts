a839995e571b67790ea3daf9ac0026784ae81568
MADUSHADEV <madusha.dev001@gmail.com>
2023-09-11 22:56:46 +0530

    cart data view / watchlist data view promotion data view section

--- js/script.js
+++ js/script.js
@@ -1,5 +1,11 @@
 const SERVER_URL = "http://localhost:9001/";
 
+document.addEventListener("DOMContentLoaded", () => {
+  cartProductView();
+  watchlistDataView();
+  productPromotionView();
+});
+
 // header
 const toggle = document.querySelector(".alg-toggle-button");
 const toggleIcon = document.querySelector(".bx-menu");
@@ -144,6 +150,31 @@ function productAddingCart() {
     });
 }
 
+//cart product View
+function cartProductView() {
+  // Fetch request
+  fetch(SERVER_URL + "backend/api/ cardView.php", {
+    method: "GET", // HTTP request method
+    headers: {
+      "Content-Type": "application/json", // Request headers
+    },
+  })
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+      return response.json(); // Parse the response body as JSON
+    })
+    .then((data) => {
+      // Handle the JSON data received from the API
+      console.log("Data from the API:", data);
+    })
+    .catch((error) => {
+      // Handle errors that occur during the Fetch request
+      console.error("Fetch error:", error);
+    });
+}
+
 //product Delete From a cart
 function cartProductDelete() {
 
@@ -210,7 +241,7 @@ function cartQtyUpdate() {
       console.error("Fetch error:", error);
     });
 }
-
+//watchlist request
 //product adding a watchlist
 function productAddingWatchlist() {
   const productItemId = document.getElementById('product_item_id');
@@ -241,6 +272,31 @@ function productAddingWatchlist() {
     });
 }
 
+//watchlist data view
+function watchlistDataView() {
+  // Fetch request
+  fetch(SERVER_URL + "backend/api/watchlistView.php", {
+    method: "GET", // HTTP request method
+    headers: {
+      "Content-Type": "application/json", // Request headers
+    },
+  })
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+      return response.json(); // Parse the response body as JSON
+    })
+    .then((data) => {
+      // Handle the JSON data received from the API
+      console.log("Data from the API:", data);
+    })
+    .catch((error) => {
+      // Handle errors that occur during the Fetch request
+      console.error("Fetch error:", error);
+    });
+}
+
 //watchlist product delete
 function watchlistProductDelete() {
   const watchlist_id = document.getElementById('watchlist_id');
@@ -254,6 +310,31 @@ function watchlistProductDelete() {
       "Content-Type": "application/x-www-form-urlencoded", // Request headers
     },
     body: data,
+  })
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+      return response.json(); // Parse the response body as JSON
+    })
+    .then((data) => {
+      // Handle the JSON data received from the API
+      console.log("Data from the API:", data);
+    })
+    .catch((error) => {
+      // Handle errors that occur during the Fetch request
+      console.error("Fetch error:", error);
+    });
+}
+
+//product promotion view section
+function productPromotionView() {
+  // Fetch request
+  fetch(SERVER_URL + "backend/api/promotionDataView.php", {
+    method: "GET", // HTTP request method
+    headers: {
+      "Content-Type": "application/json", // Request headers
+    },
   })
     .then((response) => {
       if (!response.ok) {
