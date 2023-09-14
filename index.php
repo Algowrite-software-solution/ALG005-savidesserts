93ba388237db4612186a89413ccec0e6d011fdb8
janith nirmal <113751270+janithnirmal@users.noreply.github.com>
2023-09-15 03:14:02 +0530

    testamonial section udpatedq

--- index.php
+++ index.php
@@ -261,63 +261,61 @@
     </section>
 
     <!-- testamonials -->
-    <section>
-        <div class="col-12 d-flex flex-column justify-content-center align-items-center testm-main w-100 pt-3 alg-bg-dark px-5">
-            <span class="alg-text-h2 text-white fw-bold">TESTAMONIALS</span>
-            <div class="col-12 d-flex flex-lg-row flex-column justify-content-center align-items-center m-4 ">
-                <div class="testm-card d-flex justify-content-center align-items-center m-3 alg-bg-light">
-                    <div class="p-2"><img src="https://javainstitute.edu.lk/img/faculty/dr-ishantha.jpg" class="img-fluid testm-card-img ">
+    <section class="alg-bg-dark">
+        <div class="container py-3">
+            <div class="col-12 d-flex flex-column justify-content-center align-items-center testm-main w-100 pt-3  px-5">
+                <div class="py-2">
+                    <span class="alg-text-h2 text-white fw-bold">TESTAMONIALS</span>
+                </div>
+                <div class="col-12 d-flex flex-lg-row flex-column justify-content-between align-items-center gap-3 m-2 p-0">
+                    <div class="testm-card d-flex justify-content-center align-items-center  alg-bg-light p-2">
+                        <div class="col-4 p-0">
+                            <img src="https://javainstitute.edu.lk/img/faculty/dr-ishantha.jpg" class="img-fluid testm-card-img ">
+                        </div>
+                        <div class="col-8 px-2">
+                            <p class="ld-tes-card-paragraph alg-text-dark fw-bold">"Pure nostalgia in every bite!
+                                [Your Dessert Brand Name] captures
+                                the essence of Sri Lankan sweets
+                                perfectly!
+                            </p>
+                            <h5 class="alg-text-gold">
+                                - Rajeev D.
+                            </h5>
+                        </div>
                     </div>
-                    <div class="px-3 pt-3">
-                        <p class="card-para alg-text-dark fw-bold">"Pure nostalgia in every bite!
-                            [Your Dessert Brand Name] captures
-                            the essence of Sri Lankan sweets
-                            perfectly. Each dessert is a journey
-                            back to my childhood, filled with
-                            warmth and flavour. Absolutely love
-                            it!"
-                        </p>
-                        <h5 class="alg-text-gold">
-                            - Rajeev D.
-                        </h5>
+                    <div class="testm-card d-flex justify-content-center align-items-center  alg-bg-light p-2">
+                        <div class="col-4">
+                            <img src="https://javainstitute.edu.lk/img/faculty/dr-ishantha.jpg" class="img-fluid testm-card-img ">
+                        </div>
+                        <div class="col-8 px-2">
+                            <p class="ld-tes-card-paragraph alg-text-dark fw-bold">"Pure nostalgia in every bite!
+                                [Your Dessert Brand Name] captures
+                                the essence of Sri Lankan sweets
+                                perfectly!
+                            </p>
+                            <h5 class="alg-text-gold">
+                                - Rajeev D.
+                            </h5>
+                        </div>
+                    </div>
+                    <div class="testm-card d-flex justify-content-center align-items-center  alg-bg-light p-2">
+                        <div class="col-4">
+                            <img src="https://javainstitute.edu.lk/img/faculty/dr-ishantha.jpg" class="img-fluid testm-card-img ">
+                        </div>
+                        <div class="col-8 px-2">
+                            <p class="ld-tes-card-paragraph alg-text-dark fw-bold">"Pure nostalgia in every bite!
+                                [Your Dessert Brand Name] captures
+                                the essence of Sri Lankan sweets
+                                perfectly!
+                            </p>
+                            <h5 class="alg-text-gold">
+                                - Rajeev D.
+                            </h5>
+                        </div>
                     </div>
                 </div>
-                <div class="testm-card d-flex justify-content-center align-items-center m-3 alg-bg-light">
-                    <div class="p-2"><img src="https://javainstitute.edu.lk/img/faculty/dr-ishantha.jpg" class="img-fluid testm-card-img ">
-                    </div>
-                    <div class="px-3 pt-3">
-                        <p class="card-para alg-text-dark fw-bold">"Pure nostalgia in every bite!
-                            [Your Dessert Brand Name] captures
-                            the essence of Sri Lankan sweets
-                            perfectly. Each dessert is a journey
-                            back to my childhood, filled with
-                            warmth and flavour. Absolutely love
-                            it!"
-                        </p>
-                        <h5 class="alg-text-gold">
-                            - Rajeev D.
-                        </h5>
-                    </div>
-                </div>
-                <div class="testm-card d-flex justify-content-center align-items-center m-3 alg-bg-light">
-                    <div class="p-2"><img src="https://javainstitute.edu.lk/img/faculty/dr-ishantha.jpg" class="img-fluid testm-card-img ">
-                    </div>
-                    <div class="px-3 pt-3">
-                        <p class="card-para alg-text-dark fw-bold">"Pure nostalgia in every bite!
-                            [Your Dessert Brand Name] captures
-                            the essence of Sri Lankan sweets
-                            perfectly. Each dessert is a journey
-                            back to my childhood, filled with
-                            warmth and flavour. Absolutely love
-                            it!"
-                        </p>
-                        <h5 class="alg-text-gold">
-                            - Rajeev D.
-                        </h5>
-                    </div>
-                </div>
-
             </div>
+        </div>
     </section>
 
     <!-- contacts -->
