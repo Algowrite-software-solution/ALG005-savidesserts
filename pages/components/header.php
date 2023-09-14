2ea0d85d22843470488109af1b257ef3db74aad6
MADUSHADEV <madusha.dev001@gmail.com>
2023-09-15 02:51:34 +0530

    cart product adding and delete
    finish cart product adding and delete

--- pages/components/header.php
+++ pages/components/header.php
@@ -188,7 +188,7 @@ if ($sessionManager->isLoggedIn()) {
                 <button type="button" class="" data-bs-dismiss="modal" aria-label="Close"> <i class="bx bx-x fs-5 fw-bold"></i></button>
             </div>
 
-            <div class="modal-body">
+            <div class="modal-body" >
                 <div class="row bg-black text-white fw-bold rounded-3 px-4">
                     <div class="col-12">
                         <div class="row d-flex justify-content-around alg-text-h2">
@@ -205,96 +205,23 @@ if ($sessionManager->isLoggedIn()) {
                     </div>
                 </div>
 
+                <!-- cart main container -->
+                <div class="row mt-2 gap-3" id="cartMainContainer">
+                    <!-- cart items goes here -->
+                </div>
+
                 <!-- empty watchlist -->
 
-                <!-- <div class="text-center alg-header-text alg-text-h2 mt-4 fw-bold">
-                    <span>Select your favorite sweat .........</span>
-                 </div> -->
+                <div class="text-center alg-header-text alg-text-h2 mt-4 fw-bold" id="cartEmptyContainer">
+                    <!-- empty section goes here -->
+                </div>
 
                 <!-- empty watchlsit -->
-
-                <div class="row mt-2">
-                    <div class="col-12 alg-bg-dark rounded-4">
-                        <div class="row d-flex justify-content-around align-items-center text-white p-2 px-4">
-                            <div class="col-8 col-md-8 d-flex gap-3 m-0 p-0">
-                                <img src="resources/images/watchlist_img.png" alt="watchlist_img" class="watchlsit_img mt-2 mt-md-0">
-                                <div class="lh-1 m-0 p-0">
-                                    <span class="alg-text-h2 fw-semibold">Product Title Goes Here</span><br />
-                                    <span class="alg-text-h3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt </span>
-                                </div>
-                            </div>
-                            <div class="col-3 col-lg-3 d-flex gap-3 gap-lg-5 alg-text-h3 p-0 m-0">
-                                <span>300g</span>
-                                <span>LKR 2300</span>
-                                <span class="mx-2 mx-lg-0"><i class="bi bi-trash-fill"></i></span>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="row mt-2">
-                    <div class="col-12 alg-bg-dark rounded-4">
-                        <div class="row d-flex justify-content-around align-items-center text-white p-2 px-4">
-                            <div class="col-8 col-md-8 d-flex gap-3 m-0 p-0">
-                                <img src="resources/images/watchlist_img.png" alt="watchlist_img" class="watchlsit_img mt-2 mt-md-0">
-                                <div class="lh-1 m-0 p-0">
-                                    <span class="alg-text-h2 fw-semibold">Product Title Goes Here</span><br />
-                                    <span class="alg-text-h3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt </span>
-                                </div>
-                            </div>
-                            <div class="col-3 col-lg-3 d-flex gap-3 gap-lg-5 alg-text-h3 p-0 m-0">
-                                <span>300g</span>
-                                <span>LKR 2300</span>
-                                <span class="mx-2 mx-lg-0"><i class="bi bi-trash-fill"></i></span>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-
-                <div class="row mt-2">
-                    <div class="col-12 alg-bg-dark rounded-4">
-                        <div class="row d-flex justify-content-around align-items-center text-white p-2 px-4">
-                            <div class="col-8 col-md-8 d-flex gap-3 m-0 p-0">
-                                <img src="resources/images/watchlist_img.png" alt="watchlist_img" class="watchlsit_img mt-2 mt-md-0">
-                                <div class="lh-1 m-0 p-0">
-                                    <span class="alg-text-h2 fw-semibold">Product Title Goes Here</span><br />
-                                    <span class="alg-text-h3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt </span>
-                                </div>
-                            </div>
-                            <div class="col-3 col-lg-3 d-flex gap-3 gap-lg-5 alg-text-h3 p-0 m-0">
-                                <span>300g</span>
-                                <span>LKR 2300</span>
-                                <span class="mx-2 mx-lg-0"><i class="bi bi-trash-fill"></i></span>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-
-
-
-
-
-
-
             </div>
 
 
-            <div class="row d-flex justify-content-end mx-3">
-                <div class="col-5 col-md-3  text-white alg-bg-dark rounded-4">
-                    <div class="row">
-                        <div class="col-12 text-center bg-black rounded-top rounded-4">
-                            <span class="fw-semibold">Sub Total</span>
-                        </div>
-                    </div>
-                    <div class="row">
-                        <div class="col-12 p-3">
-                            <span class="alg-text-h3">Discount 0%</span><br />
-                            <span class="alg-text-h2 fw-bold">LKR 6000</span>
-                        </div>
-                    </div>
-                </div>
+            <div class="row d-flex justify-content-end mx-3" id="cartTotalContainer">
+                <!-- Total Goes here -->
             </div>
 
         </div>
@@ -313,7 +240,7 @@ if ($sessionManager->isLoggedIn()) {
     <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable">
 
 
-        <div class="modal-content alg-bg-light" >
+        <div class="modal-content alg-bg-light">
             <div class="modal-header alg-bg-dark">
                 <h1 class="modal-title fs-5 text-white" id="staticBackdropLabel">WATCHLIST</h1>
                 <button type="button" class="" data-bs-dismiss="modal" aria-label="Close"> <i class="bx bx-x fs-5 fw-bold"></i></button>
@@ -336,7 +263,7 @@ if ($sessionManager->isLoggedIn()) {
 
                 <!-- empty watchlist -->
 
-                 <!-- <div class="text-center alg-header-text alg-text-h2 mt-4 fw-bold">
+                <!-- <div class="text-center alg-header-text alg-text-h2 mt-4 fw-bold">
                     <span>Select your favorite sweat .........</span>
                  </div> -->
 
@@ -346,7 +273,7 @@ if ($sessionManager->isLoggedIn()) {
                     <div class="col-12 alg-bg-dark rounded-4">
                         <div class="row d-flex justify-content-around align-items-center text-white m-0 p-2 px-3">
                             <div class="col-8 d-flex gap-3 m-0 p-0">
-                                <img src="resources/images/watchlist_img.png" alt="watchlist_img" class="watchlsit_img mt-3 mt-md-0"> 
+                                <img src="resources/images/watchlist_img.png" alt="watchlist_img" class="watchlsit_img mt-3 mt-md-0">
                                 <div class="lh-1">
                                     <span class="alg-text-h2 fw-semibold">Product Title Goes Here</span><br />
                                     <span class="alg-text-h3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt </span>
@@ -364,25 +291,7 @@ if ($sessionManager->isLoggedIn()) {
                     <div class="col-12 alg-bg-dark rounded-4">
                         <div class="row d-flex justify-content-around align-items-center text-white m-0 p-2 px-3">
                             <div class="col-8 d-flex gap-3 m-0 p-0">
-                                <img src="resources/images/watchlist_img.png" alt="watchlist_img" class="watchlsit_img mt-3 mt-md-0"> 
-                                <div class="lh-1">
-                                    <span class="alg-text-h2 fw-semibold">Product Title Goes Here</span><br />
-                                    <span class="alg-text-h3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt </span>
-                                </div>
-                            </div>
-                            <div class="col-3 d-flex gap-5 alg-text-h3 m-0 p-0">
-                                <span>LKR 2300</span>
-                                <span><i class="bi bi-trash-fill"></i></span>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-               
-                <div class="row mt-2">
-                    <div class="col-12 alg-bg-dark rounded-4">
-                        <div class="row d-flex justify-content-around align-items-center text-white m-0 p-2 px-3">
-                            <div class="col-8 d-flex gap-3 m-0 p-0">
-                                <img src="resources/images/watchlist_img.png" alt="watchlist_img" class="watchlsit_img mt-3 mt-md-0"> 
+                                <img src="resources/images/watchlist_img.png" alt="watchlist_img" class="watchlsit_img mt-3 mt-md-0">
                                 <div class="lh-1">
                                     <span class="alg-text-h2 fw-semibold">Product Title Goes Here</span><br />
                                     <span class="alg-text-h3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt </span>
@@ -396,8 +305,26 @@ if ($sessionManager->isLoggedIn()) {
                     </div>
                 </div>
 
-               
-              
+                <div class="row mt-2">
+                    <div class="col-12 alg-bg-dark rounded-4">
+                        <div class="row d-flex justify-content-around align-items-center text-white m-0 p-2 px-3">
+                            <div class="col-8 d-flex gap-3 m-0 p-0">
+                                <img src="resources/images/watchlist_img.png" alt="watchlist_img" class="watchlsit_img mt-3 mt-md-0">
+                                <div class="lh-1">
+                                    <span class="alg-text-h2 fw-semibold">Product Title Goes Here</span><br />
+                                    <span class="alg-text-h3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt </span>
+                                </div>
+                            </div>
+                            <div class="col-3 d-flex gap-5 alg-text-h3 m-0 p-0">
+                                <span>LKR 2300</span>
+                                <span><i class="bi bi-trash-fill"></i></span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+
+
 
             </div>
         </div>
