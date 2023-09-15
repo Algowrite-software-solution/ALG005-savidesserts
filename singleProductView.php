63442922e2b6a2ccfa5f0e5c9edca674a47abcac
janith nirmal <113751270+janithnirmal@users.noreply.github.com>
2023-09-15 17:38:59 +0530

    single product view responsive fix

--- singleProductView.php
+++ singleProductView.php
@@ -33,139 +33,143 @@
     <?php include("pages/components/header.php") ?>
 
     <!-- singleProduct -->
-    <div class="col-12 d-flex singleProduct-main">
-        <div class="flex-column flex-lg-row col-12 d-flex justify-content-center align-items-center mt-5 mb-5">
-            <div class="col-lg-5 order-2 order-lg-1 d-flex flex-column py-1 mx-5   rounded-5 justify-content-center align-items-center singleProductImg-box">
-                <div class="w-100 position-relative px-3">
-                    <div style="--swiper-navigation-color: #fff; --swiper-pagination-color: #fff" class="swiper mySwiper2">
-                        <div class="swiper-wrapper">
-                            <div class="swiper-slide">
-                                <img src="https://swiperjs.com/demos/images/nature-1.jpg" />
+    <section class="alg-bg-light">
+        <div class="container">
+            <div class="col-12 d-flex">
+                <div class="flex-column flex-lg-row col-12 d-flex justify-content-center align-items-center mt-5 mb-5">
+                    <div class="col-lg-5 col-12 order-1 d-flex flex-column py-1 mx-5   rounded-5 justify-content-center align-items-center singleProductImg-box">
+                        <div class="w-100 position-relative px-3">
+                            <div style="--swiper-navigation-color: #fff; --swiper-pagination-color: #fff" class="swiper mySwiper2">
+                                <div class="swiper-wrapper">
+                                    <div class="swiper-slide">
+                                        <img src="https://swiperjs.com/demos/images/nature-1.jpg" />
+                                    </div>
+                                    <div class="swiper-slide">
+                                        <img src="https://swiperjs.com/demos/images/nature-2.jpg" />
+                                    </div>
+                                    <div class="swiper-slide">
+                                        <img src="https://swiperjs.com/demos/images/nature-3.jpg" />
+                                    </div>
+                                    <div class="swiper-slide">
+                                        <img src="https://swiperjs.com/demos/images/nature-4.jpg" />
+                                    </div>
+                                    <div class="swiper-slide">
+                                        <img src="https://swiperjs.com/demos/images/nature-5.jpg" />
+                                    </div>
+                                    <div class="swiper-slide">
+                                        <img src="https://swiperjs.com/demos/images/nature-6.jpg" />
+                                    </div>
+                                    <div class="swiper-slide">
+                                        <img src="https://swiperjs.com/demos/images/nature-7.jpg" />
+                                    </div>
+                                    <div class="swiper-slide">
+                                        <img src="https://swiperjs.com/demos/images/nature-8.jpg" />
+                                    </div>
+                                    <div class="swiper-slide">
+                                        <img src="https://swiperjs.com/demos/images/nature-9.jpg" />
+                                    </div>
+                                    <div class="swiper-slide">
+                                        <img src="https://swiperjs.com/demos/images/nature-10.jpg" />
+                                    </div>
+                                </div>
+                                <div class="swiper-button-next"></div>
+                                <div class="swiper-button-prev"></div>
                             </div>
-                            <div class="swiper-slide">
-                                <img src="https://swiperjs.com/demos/images/nature-2.jpg" />
-                            </div>
-                            <div class="swiper-slide">
-                                <img src="https://swiperjs.com/demos/images/nature-3.jpg" />
-                            </div>
-                            <div class="swiper-slide">
-                                <img src="https://swiperjs.com/demos/images/nature-4.jpg" />
-                            </div>
-                            <div class="swiper-slide">
-                                <img src="https://swiperjs.com/demos/images/nature-5.jpg" />
-                            </div>
-                            <div class="swiper-slide">
-                                <img src="https://swiperjs.com/demos/images/nature-6.jpg" />
-                            </div>
-                            <div class="swiper-slide">
-                                <img src="https://swiperjs.com/demos/images/nature-7.jpg" />
-                            </div>
-                            <div class="swiper-slide">
-                                <img src="https://swiperjs.com/demos/images/nature-8.jpg" />
-                            </div>
-                            <div class="swiper-slide">
-                                <img src="https://swiperjs.com/demos/images/nature-9.jpg" />
-                            </div>
-                            <div class="swiper-slide">
-                                <img src="https://swiperjs.com/demos/images/nature-10.jpg" />
+                            <div thumbsSlider="" class="swiper mySwiper">
+                                <div class="swiper-wrapper">
+                                    <div class="swiper-slide">
+                                        <img src="https://swiperjs.com/demos/images/nature-1.jpg" />
+                                    </div>
+                                    <div class="swiper-slide">
+                                        <img src="https://swiperjs.com/demos/images/nature-2.jpg" />
+                                    </div>
+                                    <div class="swiper-slide">
+                                        <img src="https://swiperjs.com/demos/images/nature-3.jpg" />
+                                    </div>
+                                    <div class="swiper-slide">
+                                        <img src="https://swiperjs.com/demos/images/nature-4.jpg" />
+                                    </div>
+                                    <div class="swiper-slide">
+                                        <img src="https://swiperjs.com/demos/images/nature-5.jpg" />
+                                    </div>
+                                    <div class="swiper-slide">
+                                        <img src="https://swiperjs.com/demos/images/nature-6.jpg" />
+                                    </div>
+                                    <div class="swiper-slide">
+                                        <img src="https://swiperjs.com/demos/images/nature-7.jpg" />
+                                    </div>
+                                    <div class="swiper-slide">
+                                        <img src="https://swiperjs.com/demos/images/nature-8.jpg" />
+                                    </div>
+                                    <div class="swiper-slide">
+                                        <img src="https://swiperjs.com/demos/images/nature-9.jpg" />
+                                    </div>
+                                    <div class="swiper-slide">
+                                        <img src="https://swiperjs.com/demos/images/nature-10.jpg" />
+                                    </div>
+                                </div>
                             </div>
                         </div>
-                        <div class="swiper-button-next"></div>
-                        <div class="swiper-button-prev"></div>
                     </div>
-                    <div thumbsSlider="" class="swiper mySwiper">
-                        <div class="swiper-wrapper">
-                            <div class="swiper-slide">
-                                <img src="https://swiperjs.com/demos/images/nature-1.jpg" />
+                    <div class="col-lg-5 col-12 d-flex flex-column order-2">
+                        <div class="col-12 d-flex flex-column justify-content-center justify-content-lg-start align-items-center align-items-lg-start mb-5">
+                            <div>
+                                <h2 class="fw-bolder singleProduct-title">Watalappan Delight</h2>
                             </div>
-                            <div class="swiper-slide">
-                                <img src="https://swiperjs.com/demos/images/nature-2.jpg" />
+                            <div class="col-lg-2 my-3 singleProduct-tag text-white text-center rounded-5 p-1">
+                                Category
                             </div>
-                            <div class="swiper-slide">
-                                <img src="https://swiperjs.com/demos/images/nature-3.jpg" />
-                            </div>
-                            <div class="swiper-slide">
-                                <img src="https://swiperjs.com/demos/images/nature-4.jpg" />
-                            </div>
-                            <div class="swiper-slide">
-                                <img src="https://swiperjs.com/demos/images/nature-5.jpg" />
-                            </div>
-                            <div class="swiper-slide">
-                                <img src="https://swiperjs.com/demos/images/nature-6.jpg" />
-                            </div>
-                            <div class="swiper-slide">
-                                <img src="https://swiperjs.com/demos/images/nature-7.jpg" />
-                            </div>
-                            <div class="swiper-slide">
-                                <img src="https://swiperjs.com/demos/images/nature-8.jpg" />
-                            </div>
-                            <div class="swiper-slide">
-                                <img src="https://swiperjs.com/demos/images/nature-9.jpg" />
-                            </div>
-                            <div class="swiper-slide">
-                                <img src="https://swiperjs.com/demos/images/nature-10.jpg" />
+                            <p class="px-4 px-lg-0">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellat,
+                                molestias ipsa
+                                similique
+                                sed
+                                eaque commodi excepturi, nesciunt debitis enim, at est velit dicta nulla harum! Et natus
+                                totam
+                                nemo iste?</p>
+                            <div class="d-flex my-3 text-white p-2 px-2 singleProduct-tag justify-content-around align-items-center rounded-5">
+                                <div class="px-3 fs-6">RATING</div>
+                                <div class="bg-white d-flex rounded-end-5 p-1">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="23" height="23" viewBox="0 0 24 24" fill="yellow">
+                                        <path d="M12 .587l3.668 7.568 8.332 1.151-6.064 5.828 1.48 8.279-7.416-3.967-7.417 3.967 1.481-8.279-6.064-5.828 8.332-1.151z" />
+                                    </svg>
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="23" height="23" viewBox="0 0 24 24" fill="yellow">
+                                        <path d="M12 .587l3.668 7.568 8.332 1.151-6.064 5.828 1.48 8.279-7.416-3.967-7.417 3.967 1.481-8.279-6.064-5.828 8.332-1.151z" />
+                                    </svg>
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="23" height="23" viewBox="0 0 24 24" fill="yellow">
+                                        <path d="M12 .587l3.668 7.568 8.332 1.151-6.064 5.828 1.48 8.279-7.416-3.967-7.417 3.967 1.481-8.279-6.064-5.828 8.332-1.151z" />
+                                    </svg>
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="23" height="23" viewBox="0 0 24 24" fill="yellow">
+                                        <path d="M12 .587l3.668 7.568 8.332 1.151-6.064 5.828 1.48 8.279-7.416-3.967-7.417 3.967 1.481-8.279-6.064-5.828 8.332-1.151z" />
+                                    </svg>
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="23" height="23" viewBox="0 0 24 24" fill="yellow">
+                                        <path d="M12 .587l3.668 7.568 8.332 1.151-6.064 5.828 1.48 8.279-7.416-3.967-7.417 3.967 1.481-8.279-6.064-5.828 8.332-1.151z" />
+                                    </svg>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-5 col-12 d-flex flex-column order-1 order-lg-2">
-                <div class="col-12 d-flex flex-column justify-content-center justify-content-lg-start align-items-center align-items-lg-start mb-5">
-                    <div>
-                        <h2 class="fw-bolder singleProduct-title">Watalappan Delight</h2>
-                    </div>
-                    <div class="col-lg-2 my-3 singleProduct-tag text-white text-center rounded-5 p-1">
-                        Category
-                    </div>
-                    <p class="px-4 px-lg-0">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellat,
-                        molestias ipsa
-                        similique
-                        sed
-                        eaque commodi excepturi, nesciunt debitis enim, at est velit dicta nulla harum! Et natus
-                        totam
-                        nemo iste?</p>
-                    <div class="d-flex my-3 text-white p-2 px-2 singleProduct-tag justify-content-around align-items-center rounded-5">
-                        <div class="px-3 fs-6">RATING</div>
-                        <div class="bg-white d-flex rounded-end-5 p-1">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="23" height="23" viewBox="0 0 24 24" fill="yellow">
-                                <path d="M12 .587l3.668 7.568 8.332 1.151-6.064 5.828 1.48 8.279-7.416-3.967-7.417 3.967 1.481-8.279-6.064-5.828 8.332-1.151z" />
-                            </svg>
-                            <svg xmlns="http://www.w3.org/2000/svg" width="23" height="23" viewBox="0 0 24 24" fill="yellow">
-                                <path d="M12 .587l3.668 7.568 8.332 1.151-6.064 5.828 1.48 8.279-7.416-3.967-7.417 3.967 1.481-8.279-6.064-5.828 8.332-1.151z" />
-                            </svg>
-                            <svg xmlns="http://www.w3.org/2000/svg" width="23" height="23" viewBox="0 0 24 24" fill="yellow">
-                                <path d="M12 .587l3.668 7.568 8.332 1.151-6.064 5.828 1.48 8.279-7.416-3.967-7.417 3.967 1.481-8.279-6.064-5.828 8.332-1.151z" />
-                            </svg>
-                            <svg xmlns="http://www.w3.org/2000/svg" width="23" height="23" viewBox="0 0 24 24" fill="yellow">
-                                <path d="M12 .587l3.668 7.568 8.332 1.151-6.064 5.828 1.48 8.279-7.416-3.967-7.417 3.967 1.481-8.279-6.064-5.828 8.332-1.151z" />
-                            </svg>
-                            <svg xmlns="http://www.w3.org/2000/svg" width="23" height="23" viewBox="0 0 24 24" fill="yellow">
-                                <path d="M12 .587l3.668 7.568 8.332 1.151-6.064 5.828 1.48 8.279-7.416-3.967-7.417 3.967 1.481-8.279-6.064-5.828 8.332-1.151z" />
-                            </svg>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-12 d-flex flex-column mt-5 justify-content-center justify-content-lg-start align-items-center align-items-lg-start">
-                    <div class="col-12 d-flex justify-content-center justify-content-lg-start align-items-center align-items-lg-start">
-                        <div class="col-5">
-                            <select class="form-select" aria-label="Default select example">
-                                <option value="1">250g</option>
-                                <option value="2">300g</option>
-                                <option value="3">500g</option>
-                                <option value="3">750g</option>
-                                <option value="3">1Kg</option>
-                            </select>
-                        </div>
-                        <div class="col-5 d-flex mx-3 p-2 justify-content-center align-items-center rounded-5 singleProduct-tag text-white">
-                            LKR 2500
-                        </div>
-                    </div>
+                        <div class="col-12 d-flex flex-column mt-5 justify-content-center justify-content-lg-start align-items-center align-items-lg-start">
+                            <div class="col-12 d-flex justify-content-center justify-content-lg-start align-items-center align-items-lg-start">
+                                <div class="col-5">
+                                    <select class="form-select" aria-label="Default select example">
+                                        <option value="1">250g</option>
+                                        <option value="2">300g</option>
+                                        <option value="3">500g</option>
+                                        <option value="3">750g</option>
+                                        <option value="3">1Kg</option>
+                                    </select>
+                                </div>
+                                <div class="col-5 d-flex mx-3 p-2 justify-content-center align-items-center rounded-5 singleProduct-tag text-white">
+                                    LKR 2500
+                                </div>
+                            </div>
 
-                </div>
+                        </div>
 
+                    </div>
+                </div>
             </div>
         </div>
-    </div>
+    </section>
 
     <!-- singleProduct -->
     <section class="alg-bg-dark">
