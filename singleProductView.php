bbac6773555ab515d7bcdce11e71a90fa85b77d0
kavindu202 <kavindu.sasanka962@gmail.com>
2023-09-15 16:14:25 +0530

    single proudct view issues

--- singleProductView.php
+++ singleProductView.php
@@ -10,8 +10,7 @@
     <link rel="stylesheet" href="css/bootstrap.css">
     <link rel="stylesheet" href="css/main.css">
     <link rel="stylesheet" href="css/style.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
 
     <!-- boxicons -->
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet' />
@@ -19,139 +18,112 @@
 
 
     <!-- script -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm"
-        crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
     <script defer src="./js/bootstrap.bundle.js"></script>
     <script defer src="./js/singleProduct.js"></script>
     <script defer src="./js/script.js"></script>
 </head>
 
 <body>
-    <div class="">
-        <div class="col-12">
-            <?php include("pages/components/header.php") ?>
 
-            <!-- Start singleProduct -->
+    <div class="col-12">
+        <?php include("pages/components/header.php") ?>
 
-            <div class="col-12 d-flex singleProduct-main">
+        <!-- Start singleProduct -->
 
-                <div class="flex-column flex-lg-row col-12 d-flex justify-content-center align-items-center mt-5 mb-5">
+        <div class="col-12 d-flex singleProduct-main">
 
-                    <div
-                        class="col-lg-5 order-2 order-lg-1 d-flex flex-column pt-4 pb-4 mx-5 mt-lg-0 mt-3 rounded-5 justify-content-center align-items-center singleProductImg-box">
-                        <div class="col-11">
-                            <div class="container ">
-                                <div class="mySlides main-box ">
-                                    <img src="https://cdn.pixabay.com/photo/2017/01/11/11/33/cake-1971552_1280.jpg"
-                                        style="width:100%" class="rounded-5">
-                                </div>
+            <div class="flex-column flex-lg-row col-12 d-flex justify-content-center align-items-center mt-5 mb-5">
 
-                                <div class="mySlides">
-                                    <img src="https://cdn.pixabay.com/photo/2017/01/16/17/45/pancake-1984716_1280.jpg"
-                                        style="width:100%" class="rounded-5">
-                                </div>
-
-                                <div class="mySlides">
-                                    <img src="https://cdn.pixabay.com/photo/2017/12/01/16/14/cookies-2991174_1280.jpg"
-                                        style="width:100%" class="rounded-5">
-                                </div>
-
-                                <div class="mySlides">
-                                    <img src="https://cdn.pixabay.com/photo/2014/05/23/23/17/dessert-352475_1280.jpg"
-                                        style="width:100%" class="rounded-5">
-                                </div>
+                <div class="col-lg-5 order-2 order-lg-1 d-flex flex-column pt-4 pb-4 mx-5 mt-lg-0 mt-3 rounded-5 justify-content-center align-items-center singleProductImg-box">
+                    <div class="col-11">
+                        <div class="container ">
+                            <div class="mySlides main-box ">
+                                <img src="https://cdn.pixabay.com/photo/2017/01/11/11/33/cake-1971552_1280.jpg" style="width:100%" class="rounded-5">
                             </div>
 
-                            <div class="d-flex flex-row mt-4">
-                                <div class="">
-                                    <img class="demo cursor"
-                                        src="https://cdn.pixabay.com/photo/2017/01/11/11/33/cake-1971552_1280.jpg"
-                                        style="width:100%" onclick="currentSlide(1)" alt="The Woods">
-                                </div>
-                                <div class="">
-                                    <img class="demo cursor"
-                                        src="https://cdn.pixabay.com/photo/2017/01/16/17/45/pancake-1984716_1280.jpg"
-                                        style="width:100%" onclick="currentSlide(2)" alt="Cinque Terre">
-                                </div>
-                                <div class="">
-                                    <img class="demo cursor"
-                                        src="https://cdn.pixabay.com/photo/2017/12/01/16/14/cookies-2991174_1280.jpg"
-                                        style="width:100%" onclick="currentSlide(3)" alt="Mountains and fjords">
-                                </div>
-                                <div class="">
-                                    <img class="demo cursor"
-                                        src="https://cdn.pixabay.com/photo/2014/05/23/23/17/dessert-352475_1280.jpg"
-                                        style="width:100%" onclick="currentSlide(4)" alt="Northern Lights">
-                                </div>
-
+                            <div class="mySlides">
+                                <img src="https://cdn.pixabay.com/photo/2017/01/16/17/45/pancake-1984716_1280.jpg" style="width:100%" class="rounded-5">
                             </div>
 
+                            <div class="mySlides">
+                                <img src="https://cdn.pixabay.com/photo/2017/12/01/16/14/cookies-2991174_1280.jpg" style="width:100%" class="rounded-5">
+                            </div>
+
+                            <div class="mySlides">
+                                <img src="https://cdn.pixabay.com/photo/2014/05/23/23/17/dessert-352475_1280.jpg" style="width:100%" class="rounded-5">
+                            </div>
+                        </div>
+
+                        <div class="d-flex flex-row mt-4">
+                            <div class="">
+                                <img class="demo cursor" src="https://cdn.pixabay.com/photo/2017/01/11/11/33/cake-1971552_1280.jpg" style="width:100%" onclick="currentSlide(1)" alt="The Woods">
+                            </div>
+                            <div class="">
+                                <img class="demo cursor" src="https://cdn.pixabay.com/photo/2017/01/16/17/45/pancake-1984716_1280.jpg" style="width:100%" onclick="currentSlide(2)" alt="Cinque Terre">
+                            </div>
+                            <div class="">
+                                <img class="demo cursor" src="https://cdn.pixabay.com/photo/2017/12/01/16/14/cookies-2991174_1280.jpg" style="width:100%" onclick="currentSlide(3)" alt="Mountains and fjords">
+                            </div>
+                            <div class="">
+                                <img class="demo cursor" src="https://cdn.pixabay.com/photo/2014/05/23/23/17/dessert-352475_1280.jpg" style="width:100%" onclick="currentSlide(4)" alt="Northern Lights">
+                            </div>
+
+                        </div>
+
+                    </div>
+                </div>
+                <div class="col-lg-5 col-12 d-flex flex-column order-1 order-lg-2">
+                    <div class="col-12 d-flex flex-column justify-content-center justify-content-lg-start align-items-center align-items-lg-start mb-5">
+                        <div>
+                            <h2 class="fw-bolder singleProduct-title">Watalappan Delight</h2>
+                        </div>
+                        <div class="col-lg-2 my-3 singleProduct-tag text-white text-center rounded-5 p-1">
+                            Category
+                        </div>
+                        <p class="px-4 px-lg-0">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellat,
+                            molestias ipsa
+                            similique
+                            sed
+                            eaque commodi excepturi, nesciunt debitis enim, at est velit dicta nulla harum! Et natus
+                            totam
+                            nemo iste?</p>
+                        <div class="d-flex my-3 text-white p-2 px-2 singleProduct-tag justify-content-around align-items-center rounded-5">
+                            <div class="px-3 fs-6">RATING</div>
+                            <div class="bg-white d-flex rounded-end-5 p-1">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="23" height="23" viewBox="0 0 24 24" fill="yellow">
+                                    <path d="M12 .587l3.668 7.568 8.332 1.151-6.064 5.828 1.48 8.279-7.416-3.967-7.417 3.967 1.481-8.279-6.064-5.828 8.332-1.151z" />
+                                </svg>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="23" height="23" viewBox="0 0 24 24" fill="yellow">
+                                    <path d="M12 .587l3.668 7.568 8.332 1.151-6.064 5.828 1.48 8.279-7.416-3.967-7.417 3.967 1.481-8.279-6.064-5.828 8.332-1.151z" />
+                                </svg>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="23" height="23" viewBox="0 0 24 24" fill="yellow">
+                                    <path d="M12 .587l3.668 7.568 8.332 1.151-6.064 5.828 1.48 8.279-7.416-3.967-7.417 3.967 1.481-8.279-6.064-5.828 8.332-1.151z" />
+                                </svg>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="23" height="23" viewBox="0 0 24 24" fill="yellow">
+                                    <path d="M12 .587l3.668 7.568 8.332 1.151-6.064 5.828 1.48 8.279-7.416-3.967-7.417 3.967 1.481-8.279-6.064-5.828 8.332-1.151z" />
+                                </svg>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="23" height="23" viewBox="0 0 24 24" fill="yellow">
+                                    <path d="M12 .587l3.668 7.568 8.332 1.151-6.064 5.828 1.48 8.279-7.416-3.967-7.417 3.967 1.481-8.279-6.064-5.828 8.332-1.151z" />
+                                </svg>
+                            </div>
                         </div>
                     </div>
-                    <div class="col-lg-5 col-12 d-flex flex-column order-1 order-lg-2">
-                        <div
-                            class="col-12 d-flex flex-column justify-content-center justify-content-lg-start align-items-center align-items-lg-start mb-5">
-                            <div>
-                                <h2 class="fw-bolder singleProduct-title">Watalappan Delight</h2>
+                    <div class="col-12 d-flex flex-column mt-5 justify-content-center justify-content-lg-start align-items-center align-items-lg-start">
+                        <div class="col-12 d-flex justify-content-center justify-content-lg-start align-items-center align-items-lg-start">
+                            <div class="col-5">
+                                <select class="form-select" aria-label="Default select example">
+                                    <option value="1">250g</option>
+                                    <option value="2">300g</option>
+                                    <option value="3">500g</option>
+                                    <option value="3">750g</option>
+                                    <option value="3">1Kg</option>
+                                </select>
                             </div>
-                            <div class="col-lg-2 my-3 singleProduct-tag text-white text-center rounded-5 p-1">
-                                Category
-                            </div>
-                            <p class="px-4 px-lg-0">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellat,
-                                molestias ipsa
-                                similique
-                                sed
-                                eaque commodi excepturi, nesciunt debitis enim, at est velit dicta nulla harum! Et natus
-                                totam
-                                nemo iste?</p>
-                            <div
-                                class="d-flex my-3 text-white p-2 px-2 singleProduct-tag justify-content-around align-items-center rounded-5">
-                                <div class="px-3 fs-6">RATING</div>
-                                <div class="bg-white d-flex rounded-end-5 p-1">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="23" height="23" viewBox="0 0 24 24"
-                                        fill="yellow">
-                                        <path
-                                            d="M12 .587l3.668 7.568 8.332 1.151-6.064 5.828 1.48 8.279-7.416-3.967-7.417 3.967 1.481-8.279-6.064-5.828 8.332-1.151z" />
-                                    </svg>
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="23" height="23" viewBox="0 0 24 24"
-                                        fill="yellow">
-                                        <path
-                                            d="M12 .587l3.668 7.568 8.332 1.151-6.064 5.828 1.48 8.279-7.416-3.967-7.417 3.967 1.481-8.279-6.064-5.828 8.332-1.151z" />
-                                    </svg>
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="23" height="23" viewBox="0 0 24 24"
-                                        fill="yellow">
-                                        <path
-                                            d="M12 .587l3.668 7.568 8.332 1.151-6.064 5.828 1.48 8.279-7.416-3.967-7.417 3.967 1.481-8.279-6.064-5.828 8.332-1.151z" />
-                                    </svg>
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="23" height="23" viewBox="0 0 24 24"
-                                        fill="yellow">
-                                        <path
-                                            d="M12 .587l3.668 7.568 8.332 1.151-6.064 5.828 1.48 8.279-7.416-3.967-7.417 3.967 1.481-8.279-6.064-5.828 8.332-1.151z" />
-                                    </svg>
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="23" height="23" viewBox="0 0 24 24"
-                                        fill="yellow">
-                                        <path
-                                            d="M12 .587l3.668 7.568 8.332 1.151-6.064 5.828 1.48 8.279-7.416-3.967-7.417 3.967 1.481-8.279-6.064-5.828 8.332-1.151z" />
-                                    </svg>
-                                </div>
+                            <div class="col-5 d-flex mx-3 p-2 justify-content-center align-items-center rounded-5 singleProduct-tag text-white">
+                                LKR 2500
                             </div>
                         </div>
-                        <div class="col-12 d-flex flex-column mt-5 justify-content-center justify-content-lg-start align-items-center align-items-lg-start">
-                            <div class="col-12 d-flex justify-content-center justify-content-lg-start align-items-center align-items-lg-start">
-                                <div class="col-5">
-                                    <select class="form-select" aria-label="Default select example">
-                                        <option value="1">250g</option>
-                                        <option value="2">300g</option>
-                                        <option value="3">500g</option>
-                                        <option value="3">750g</option>
-                                        <option value="3">1Kg</option>
-                                    </select>
-                                </div>
-                                <div class="col-5 d-flex mx-3 p-2 justify-content-center align-items-center rounded-5 singleProduct-tag text-white">
-                                    LKR 2500
-                                </div>
-                            </div>
 
                     </div>
 
@@ -195,10 +167,12 @@
                             </div>
                         </div>
                     </div>
-                </section>
-                <?php include("pages/components/footer.php") ?>
-            </div>
+                </div>
+            </section>
+            <?php include("pages/components/footer.php") ?>
+
         </div>
+    </div>
 </body>
 
 </html>
