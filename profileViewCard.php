caeffddf82fdc346872af151b98eea36a60051d2
kavindu202 <kavindu.sasanka962@gmail.com>
2023-09-19 00:49:54 +0530

    user profile update

--- profileViewCard.php
+++ profileViewCard.php
@@ -25,7 +25,7 @@
 
 <body>
 
-    <section class="alg-bg-light">
+    <section class="alg-bg-light profile-pg">
 
         <?php include("pages/components/header.php") ?>
         <?php include("pages/components/profileNavigation.php") ?>
