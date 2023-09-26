740a7b9e76c6a060314c81fa069e94171390b2f9
kavindu202 <kavindu.sasanka962@gmail.com>
2023-09-26 10:28:29 +0530

    user statesus 1.0.1

--- backend/api/useStatesUpdate.php
+++ backend/api/useStatesUpdate.php
@@ -2,7 +2,7 @@
 
 // all user status change
 // by kavindu sasanka
-// version - 1.0.0
+// version - 1.0.1
 // 25-09-2023
 
 
@@ -18,20 +18,26 @@ header("Content-Type: application/json; charset=UTF-8");
 $responseObject = new stdClass();
 $responseObject->status = 'failed';
 
-$user_id = $_GET["id"];
+$user_id = $_GET["u_id"];
+$status_id = $_GET["s_id"];
 
-// check user id
+// check user id and ststus id
 if(empty($user_id)){
     $responseObject->status = "empty user id";
+    response_sender::sendJson($responseObject);
+}else if(empty($status_id)){
+    $responseObject->status = "empty status id";
     response_sender::sendJson($responseObject);
 }
 
 // db connection
 $db = new database_driver();
 
-$update_query = "UPDATE `user` SET `status_id`= '3'  WHERE `user_id`=?";
-$db->execute_query($update_query,'i',array($user_id));
+$update_query = "UPDATE `user` SET `status_id`= ?  WHERE `user_id`=?";
+$db->execute_query($update_query,'ii',array($status_id,$user_id));
 
 
 $responseObject->status = "Updated";
 response_sender::sendJson($responseObject);
+
+
