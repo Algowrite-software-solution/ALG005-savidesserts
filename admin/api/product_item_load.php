2d7ffc489f1493348faf21298059d0ac72d6e938
MADUSHADEV <madusha.dev001@gmail.com>
2023-10-03 17:22:52 +0530

    product item load API bug fix

--- admin/api/product_item_load.php
+++ admin/api/product_item_load.php
@@ -18,11 +18,11 @@ $responseObject = new stdClass();
 $responseObject->status = 'failed';
 
 // chekcing is user logging
-// $userCheckSession = new SessionManager();
-// if (!$userCheckSession->isLoggedIn() || !$userCheckSession->getUserId()) {
-//      $responseObject->error = 'Please LogIn';
-//      response_sender::sendJson($responseObject);
-// }
+$userCheckSession = new SessionManager();
+if (!$userCheckSession->isLoggedIn() || !$userCheckSession->getUserId()) {
+     $responseObject->error = 'Please LogIn';
+     response_sender::sendJson($responseObject);
+}
 
 //database object
 $db = new database_driver();
