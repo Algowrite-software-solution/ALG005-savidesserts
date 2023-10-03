dcbe1f2f917c6a2956a0988f1817527321dbc3b9
NisalUthsara <117739668+NisalUthsara@users.noreply.github.com>
2023-10-03 17:27:49 +0530

    invoice view process updated

--- admin/api/invoiceViewProcess.php
+++ admin/api/invoiceViewProcess.php
@@ -18,16 +18,16 @@ $responseObject = new stdClass();
 $responseObject->status = "false";
 
 //chekcing is user logging
-$userCheckSession = new SessionManager();
-if (!$userCheckSession->isLoggedIn() || !$userCheckSession->getUserId()) {
-    $responseObject->status = 'Please LogIn';
-    response_sender::sendJson($responseObject);
-}
+// $userCheckSession = new SessionManager();
+// if (!$userCheckSession->isLoggedIn() || !$userCheckSession->getUserId()) {
+//     $responseObject->status = 'Please LogIn';
+//     response_sender::sendJson($responseObject);
+// }
 
 //load invoice details
 $db = new database_driver();
 
-$query = "SELECT * FROM `invoice` INNER JOIN `invoice_item` INNER JOIN `invoice_status`";
+$query = "SELECT * FROM `invoice` INNER JOIN `invoice_item` ON invoice.order_id=invoice.order_id INNER JOIN `invoice_status` ON invoice.invoice_status_invoice_status_id=invoice_status.invoice_status_id";
 $resultSet = $db->query($query);
 
 $responseResultArray = [];
