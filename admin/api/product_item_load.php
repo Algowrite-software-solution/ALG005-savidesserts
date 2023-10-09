328d02f1cda302874a744fdf3244d498e05e3fe2
MADUSHADEV <madusha.dev001@gmail.com>
2023-10-09 22:45:17 +0530

    product item images  include array

--- admin/api/product_item_load.php
+++ admin/api/product_item_load.php
@@ -56,17 +56,17 @@ if ($resultSet->num_rows > 0) {
         $resRowDetailObject->product_status_id = $rowData['product_status_id'];
         $resRowDetailObject->product_id = $productId;
         $resRowDetailObject->weight_id = $weightId;
-         
 
-        $imageArray = [];
+
+        $imageArray = array();
         // Add images to the new object if available
         if (is_array($searchResults)) {
             foreach ($searchResults as $index => $searchResult) {
-                $resRowDetailObject->{"images[$index]"} = $searchResult;
-                $imageArray.array_push($searchResult);
-                
+                // $resRowDetailObject->{"images[$index]"} = $searchResult;
+                array_push($imageArray, $searchResult);
             }
         }
+        $resRowDetailObject->images = $imageArray;
 
         // Check if there's an existing object with the same productId and weightId
         $key = "{$productId}_{$weightId}";
