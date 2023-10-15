4e133e9940e9cd032c0f6986c6366e593c7a2872
janith nirmal <113751270+janithnirmal@users.noreply.github.com>
2023-10-15 20:31:03 +0530

    checkout details design optimized

--- bin/paymentCheckout.php
+++ bin/paymentCheckout.php
@@ -42,9 +42,9 @@
 </head>
 
 <body class="alg-bg-light">
-    <div class="container bg-primary pb-3 pt-3">
+    <div class="container my-3 pb-3 pt-3">
         <!-- topic -->
-        <div class="d-flex pt-2 pb-2 bg-body justify-content-center justify-content-lg-start">
+        <div class="d-flex pt-2 pb-2 alg-text-dark justify-content-center justify-content-lg-start">
             <h1>Checkout Details</h1>
         </div>
 
@@ -52,7 +52,7 @@
         <div class="d-flex flex-column flex-lg-row gap-2  justify-content-between">
 
             <!-- first div -->
-            <div class="bg-warning d-flex flex-column gap-2 flex-grow-1 p-3">
+            <div class="alg-bg-dark alg-text-light alg-rounded-small d-flex flex-column gap-2 flex-grow-1 p-3">
 
                 <!-- topic -->
                 <div>
@@ -82,51 +82,50 @@
 
                 <div class="d-flex flex-column flex-md-row flex-lg-row justify-content-end gap-2">
                     <!-- button section -->
-                    <button class="fw-bolder btn btn-danger" onclick="EditBtnWorker();">Edit</button>
+                    <button class="fw-bolder btn alg-btn-pill" onclick="EditBtnWorker();">Edit</button>
 
-                    <button id="saveBtn" class="fw-bolder  btn btn-primary" onclick="updateShippingData();" disabled>Save</button>
+                    <button id="saveBtn" class="fw-bolder  btn alg-btn-pill" onclick="updateShippingData();" disabled>Save</button>
                 </div>
 
             </div>
 
             <!-- second dev -->
-            <div class="alg-bg-tan flex-grow-1 p-3 ">
+            <div class="alg-bg-dark alg-rounded-small flex-grow-1 p-3 ">
 
                 <!-- topic -->
-                <div class="bg-black text-white">
+                <div class="alg-text-light">
                     <h2>Your Order</h2>
                 </div>
 
 
                 <!-- your order detail section -->
-                <div class="bg-info d-flex gap-3 flex-column h-auto scroll-container pb-3">
-
-                    <div class="d-flex gap-2 bg-secondary">
+                <div class="alg-text-light d-flex gap-3 flex-column h-auto  pb-3">
+                    <div class="d-flex flex-column flex-lg-row gap-2">
                         <!-- product image -->
-                        <img src="https://img.taste.com.au/6i4vEH8z/w354-h236-cfill-q80/taste/2016/11/warm-chocolate-puddings-22259-1.jpeg" alt="you order images" class="alg-pc-img">
+                        <img class="alg-rounded-small" src="https://img.taste.com.au/6i4vEH8z/w354-h236-cfill-q80/taste/2016/11/warm-chocolate-puddings-22259-1.jpeg" alt="you order images" class="alg-pc-img">
 
                         <!-- details goes here -->
-                        <div class="bg-warning d-flex flex-column flex-grow-1">
+                        <div class=" d-flex flex-column flex-grow-1">
                             <!-- product name -->
-                            <div class="bg-secondary">
+                            <div class="">
                                 <h4 class="fw-bold">product name goes here</h4>
                             </div>
-                            <div class="d-flex gap-2 ">
+                            <div class="d-flex gap-2 overflow-auto">
                                 <!-- one side -->
-                                <div class="bg-danger-subtle d-flex flex-column flex-grow-1 p-2">
+                                <div class="alg-bg-tan alg-text-dark alg-rounded-small d-flex flex-column flex-grow-1 p-2">
                                     <span>Weight: 1kg</span>
                                     <span>QTY: 1</span>
                                     <span>Topping: white chocolate</span>
                                 </div>
 
                                 <!-- other side -->
-                                <div class="bg-info-subtle d-flex flex-column flex-grow-1 p-2">
+                                <div class="alg-bg-tan alg-text-dark alg-rounded-small d-flex flex-column flex-grow-1 p-2">
                                     <span>Product Price : Rs.2000</span>
                                     <span>Topping Price : Rs.300</span>
 
                                 </div>
                                 <!-- other side -->
-                                <div class="bg-info-subtle d-flex flex-column flex-grow-1 p-2">
+                                <div class="alg-bg-tan alg-text-dark alg-rounded-small d-flex flex-column flex-grow-1 p-2">
                                     <span class="fw-bold">Rs.2300</span>
                                 </div>
                             </div>
@@ -135,23 +134,23 @@
                 </div>
 
                 <!-- prices -->
-                <div class="bg-body-tertiary d-flex flex-column flex-lg-row  gap-2 pe-0 pe-lg-2 p-2">
+                <div class=" d-flex flex-column flex-lg-row  gap-2 pe-0 pe-lg-2 py-2">
 
 
                     <!-- payment Method -->
-                    <div class="d-flex flex-row flex-lg-column flex-grow-1  bg-danger">
+                    <div class="d-flex flex-row flex-lg-column flex-grow-1  alg-bg-darker alg-text-light p-3 alg-rounded-small">
                         <h4>Estimate Delivery Date</h4>
                     </div>
 
-                    <div class="d-flex flex-grow-1 justify-content-lg-end justify-content-center">
+                    <div class="d-flex flex-grow-1 justify-content-lg-end justify-content-center gap-2">
                         <!-- side names -->
-                        <div class="bg-primary d-flex flex-grow-1 flex-column gap-1">
+                        <div class="alg-bg-light alg-text-dark p-3 alg-rounded-small d-flex flex-grow-1 flex-column gap-1">
                             <span class="">Sub Total :</span>
                             <span class="">Shipping Price :</span>
                             <span class="fw-bold ">TOTAL TO PAY :</span>
                         </div>
                         <!-- prices goes here -->
-                        <div class="bg-warning d-flex flex-grow-1 flex-column gap-1 pe-0 pe-lg-3">
+                        <div class="p-3 alg-rounded-small alg-bg-light d-flex flex-grow-1 flex-column gap-1 pe-0 pe-lg-3">
                             <span class="">Rs.2300.00</span>
                             <span class="">Rs. 200.00</span>
                             <span class="fw-bolder">Rs.2500.00</span>
