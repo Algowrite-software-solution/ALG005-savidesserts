11594b5af7bc265076732252110d222b73864d9b
MADUSHADEV <madusha.dev001@gmail.com>
2023-10-15 12:44:58 +0530

    chackout new UI

--- bin/paymentCheckout.php
+++ bin/paymentCheckout.php
@@ -22,13 +22,12 @@
     <title>ALG005 - savi dessert</title>
 
     <!-- css -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@10/swiper-bundle.min.css" />
     <link rel="stylesheet" href="../css/bootstrap.css">
     <link rel="stylesheet" href="../css/main.css">
     <link rel="stylesheet" href="../css/singleProduct.css">
-    <link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" href="style.css">
 
     <!-- boxicons -->
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet' />
@@ -36,125 +35,148 @@
 
 
     <!-- script -->
-    <script defer src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm"
-        crossorigin="anonymous"></script>
+    <script defer src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
     <script defer src="https://cdn.jsdelivr.net/npm/swiper@10/swiper-bundle.min.js"></script>
     <script defer src="../js/bootstrap.bundle.js"></script>
     <script defer src="../js/homeSliders.js"></script>
 </head>
 
-<body>
-    <!-- <?php include("pages/components/header.php") ?> -->
-    <div class="container-fluid flex-column flex-lg-row m-0 p-0 d-flex paycheck-main ">
-        <div class="col-12 col-lg-6 d-flex flex-column alg-bg-light">
-            <div class="d-flex justify-content-center justify-content-lg-start p-3">
-                <h2 class="alg-text-dark fw-bolder">Checkout Details</h2>
+<body class="alg-bg-light">
+    <div class="container bg-primary pb-3 pt-3">
+        <!-- topic -->
+        <div class="d-flex pt-2 pb-2 bg-body justify-content-center justify-content-lg-start">
+            <h1>Checkout Details</h1>
+        </div>
+
+        <!-- main div -->
+        <div class="d-flex flex-column flex-lg-row gap-2  justify-content-between">
+
+            <!-- first div -->
+            <div class="bg-warning d-flex flex-column gap-2 flex-grow-1 p-3">
+
+                <!-- topic -->
+                <div>
+                    <h3>Shipping Details</h3>
+                </div>
+                <!-- details -->
+                <!-- full name input -->
+                <input id="fullName" placeholder="Full Name" type="text" class="form-control" disabled />
+                <!-- mobile input -->
+                <input id="mobile" placeholder="Mobile" type="tel" class="form-control" disabled />
+
+                <!-- address 1 input -->
+                <input id="addressLine1" placeholder="Address Line 1" type="text" class="form-control" disabled />
+                <!-- address 2 input -->
+                <input id="addressLine2" placeholder="Address Line 2 (Optional)" type="text" class="form-control" disabled />
+
+                <!-- city -->
+                <input id="city" placeholder="City" type="text" class="form-control" disabled />
+                <!-- selector -->
+                <select id="district" class="form-select" disabled>
+                    <!-- district option goes here -->
+                </select>
+                <!-- selector -->
+                <select id="province" class="form-select" disabled>
+                    <!-- province option goes here -->
+                </select>
+
+                <div class="d-flex flex-column flex-md-row flex-lg-row justify-content-end gap-2">
+                    <!-- button section -->
+                    <button class="fw-bolder btn btn-danger" onclick="EditBtnWorker();">Edit</button>
+
+                    <button id="saveBtn" class="fw-bolder  btn btn-primary" onclick="updateShippingData();" disabled>Save</button>
+                </div>
+
             </div>
-            <div class="col-12 d-flex flex-column justify-content-center p-1 p-lg-3">
-                <h4 class="alg-text-dark py-4">Shipping Details</h4>
-                <div class="d-flex flex-column justify-content-between alg-bg-dark rounded-5 p-4 px-3 gap-3 shadow">
-                    <div class="d-flex flex-column flex-md-row flex-lg-row justify-content-between gap-2"
-                        id="container1">
-                        <input id="fullName" placeholder="Full Name" type="text" class="form-control" disabled />
-                        <input id="mobile" placeholder="Mobile" type="tel" class="form-control" disabled />
-                    </div>
-                    <div class="d-flex flex-column  flex-md-row flex-lg-row justify-content-between gap-2"
-                        id="container2">
-                        <input id="addressLine1" placeholder="Address Line 1" type="text" class="form-control"
-                            disabled />
-                        <input id="addressLine2" placeholder="Address Line 2 (Optional)" type="text"
-                            class="form-control" disabled />
-                    </div>
-                    <div class="d-flex flex-column flex-md-row flex-lg-row justify-content-between gap-2"
-                        id="container3">
-                        <input id="city" placeholder="City" type="text" class="form-control" disabled />
-                        <select id="district" class="form-select" disabled>
-                            <!-- district option goes here -->
-                        </select>
-                        <select id="province" class="form-select" disabled>
-                            <!-- province option goes here -->
-                        </select>
-                    </div>
-                    <div class="d-flex flex-column flex-md-row flex-lg-row justify-content-end gap-2">
-                        <button class="fw-bolder btn btn-danger" onclick="EditBtnWorker();">Edit</button>
-                        <button id="saveBtn" class="fw-bolder  btn btn-primary" onclick="updateShippingData();"
-                            disabled>Save</button>
+
+            <!-- second dev -->
+            <div class="alg-bg-tan flex-grow-1 p-3 ">
+
+                <!-- topic -->
+                <div class="bg-black text-white">
+                    <h2>Your Order</h2>
+                </div>
+
+
+                <!-- your order detail section -->
+                <div class="bg-info d-flex gap-3 flex-column h-auto scroll-container pb-3">
+
+                    <div class="d-flex gap-2 bg-secondary">
+                        <!-- product image -->
+                        <img src="https://img.taste.com.au/6i4vEH8z/w354-h236-cfill-q80/taste/2016/11/warm-chocolate-puddings-22259-1.jpeg" alt="you order images" class="alg-pc-img">
+
+                        <!-- details goes here -->
+                        <div class="bg-warning d-flex flex-column flex-grow-1">
+                            <!-- product name -->
+                            <div class="bg-secondary">
+                                <h4 class="fw-bold">product name goes here</h4>
+                            </div>
+                            <div class="d-flex gap-2 ">
+                                <!-- one side -->
+                                <div class="bg-danger-subtle d-flex flex-column flex-grow-1 p-2">
+                                    <span>Weight: 1kg</span>
+                                    <span>QTY: 1</span>
+                                    <span>Topping: white chocolate</span>
+                                </div>
+
+                                <!-- other side -->
+                                <div class="bg-info-subtle d-flex flex-column flex-grow-1 p-2">
+                                    <span>Product Price : Rs.2000</span>
+                                    <span>Topping Price : Rs.300</span>
+
+                                </div>
+                                <!-- other side -->
+                                <div class="bg-info-subtle d-flex flex-column flex-grow-1 p-2">
+                                    <span class="fw-bold">Rs.2300</span>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div class="col-12 d-flex flex-column justify-content-center p-1 p-lg-3">
-                <h4 class="alg-text-dark py-4">Payment Method</h4>
-                <div class="col-12 d-flex justify-content-between alg-bg-dark rounded-5 p-3 px-3 gap-3 shadow">
-                    <div class="d-flex gap-3 justify-content-center flex-row ">
-                        <img src="https://www.clearhaus.com/img/figurehead/assets/card-schemes.jpg"
-                            class="img-fluid paycheck-prof-img" alt="">
-                        <div class="d-flex alg-text-light justify-content-center flex-column">
-                            <h5 class="fs-5">Visa / Master Card</h5>
+
+                <!-- prices -->
+                <div class="bg-body-tertiary d-flex flex-column flex-lg-row  gap-2 pe-0 pe-lg-2 p-2">
+
+
+                    <!-- payment Method -->
+                    <div class="d-flex flex-row flex-lg-column flex-grow-1  bg-danger">
+                        <h4>Estimate Delivery Date</h4>
+                    </div>
+
+                    <div class="d-flex flex-grow-1 justify-content-lg-end justify-content-center">
+                        <!-- side names -->
+                        <div class="bg-primary d-flex flex-grow-1 flex-column gap-1">
+                            <span class="">Sub Total :</span>
+                            <span class="">Shipping Price :</span>
+                            <span class="fw-bold ">TOTAL TO PAY :</span>
+                        </div>
+                        <!-- prices goes here -->
+                        <div class="bg-warning d-flex flex-grow-1 flex-column gap-1 pe-0 pe-lg-3">
+                            <span class="">Rs.2300.00</span>
+                            <span class="">Rs. 200.00</span>
+                            <span class="fw-bolder">Rs.2500.00</span>
                         </div>
                     </div>
 
-                </div>
-            </div>
-        </div>
-        <div class="col-12 col-lg-6 alg-bg-light">
-            <div class="d-flex alg-bg-tan flex-column justify-content-center m-2 p-4 p-lg-5 rounded-5 shadow-lg">
-                <div class="d-flex alg-text-light justify-content-between">
-                    <h4>Your order</h4>
-                </div>
-                <!-- <div class="d-flex flex-row justify-content-around py-4 border-bottom"> -->
-                <!--purchased product slider -->
 
-                <!-- <div class="checkoutSwiper swiper mySwiperCheckOut pt-4 px-5 border-bottom">
-                    <div class="swiper-wrapper pt-4 d-flex gap-4" id="swiperDetailContainer">
-                       
-                    </div>
-                    <div class="swiper-button-next alg-text-gold"></div>
-                    <div class="swiper-button-prev alg-text-gold"></div>
-                </div> -->
+                </div>
 
-                <div
-                    class="checkoutSwiper col-3 overflow-auto bg-danger swiper-slide alg-text-light d-flex flex-column justify-content-start align-items-center">
-                    <img class="img-fluid paycheck-product-im"
-                        src="https://media.istockphoto.com/id/1179207306/photo/pudding-caramel-custard-with-caramel-sauce-and-mint-leaf-isolated-on-white-background.jpg?s=612x612&w=0&k=20&c=QOgo1aIuavOspqKTbKz7Qk2O5wJJOZZPg4fiPg0p2xM="
-                        alt="">
-                    <span class="pt-1 fw-bolder alg-text-h3">${element.product_name}</span>
-                    <span class="alg-text-h3">RS.${element.price}</span>
-                    <span class="alg-text-h3">Weight : ${element.weight}</span>
-                    <span class="alg-text-h3">Toppings : ${element.extra_fruit}</span>
-                    <span class="alg-text-h3">Toppings Price : ${element.extra_price}</span>
-                    <span class="alg-text-h3">QTY : ${element.qty}</span>
+                <!-- order button -->
+                <div class="bg-black d-flex justify-content-end flex-grow-1">
+                    <button class="alg-bg-gold w-100 alg-text-h2 border-0 rounded-3 p-2 fw-bolder">Place Order</button>
                 </div>
 
 
-                <!--purchased product slider -->
-                <!-- </div> -->
-                <div
-                    class="d-flex py-5 flex-column flex-lg-row justify-content-center align-items-center justify-content-lg-around border-bottom">
-                    <img class="img-fluid paycheck-thanku-img "
-                        src="https://img.freepik.com/free-vector/thank-you-placard-concept-illustration_114360-13436.jpg?w=996&t=st=1694928554~exp=1694929154~hmac=b5cf8b7b5d163da7bc470ecf12e411fb11ba50a1fe07d773b469898eda55e21d"
-                        alt="">
-                    <div class="col-12 pt-3 pt-lg-0 col-lg-6 text-white" id="priceContainer">
-
-                    </div>
-                </div>
-                <div class="d-flex justify-content-center justify-content-lg-end pt-4">
-                    <button onclick="placeOrder();" id="payhere-payment" type="button"
-                        class="alg-bg-gold w-100 alg-text-h2 border-0 rounded-3 p-2 fw-bolder"> Place
-                        Order
-                    </button>
-                </div>
             </div>
         </div>
     </div>
+
     <!-- toast mode -->
     <!-- <?php include("pages/components/toastMessage.php") ?> -->
 
     <script src="../js/script.js"></script>
     <script src="../js/paymentCheckout.js"></script>
     <script type="text/javascript" src="https://www.payhere.lk/lib/payhere.js"></script>
-
-    <!-- <?php include("pages/components/footer.php") ?> -->
 </body>
 
 </html>
