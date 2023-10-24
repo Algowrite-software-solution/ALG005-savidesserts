6cc9c74975a9e289322d007946a741b0049e2739
kavindu202 <kavindu.sasanka962@gmail.com>
2023-10-24 15:50:34 +0530

    thanks section

---
+++ thanks.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ALG005 - savi dessert</title>
+
+    <!-- css -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@10/swiper-bundle.min.css" />
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet' />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css" />
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="css/style.css">
+
+    <!-- script -->
+    <script defer src="https://cdn.jsdelivr.net/npm/swiper@10/swiper-bundle.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+    <script defer src="js/home.js"></script>
+    <script defer src="js/homeSliders.js"></script>
+    <script defer src="js/script.js"></script>
+</head>
+
+<body>
+
+    <section class="alg-bg-light">
+        <div class="container">
+            <div class="d-flex justify-content-center align-items-center" style="height:100vh;">
+                <img src="resources/images/thanks_img.jpg" alt="" class="thanks_img opacity-25 position-absolute" />
+                <div class="d-flex flex-column gap-2 gap-md-3 gap-lg-4">
+                    <div class="text-center"> 
+                        <span class="alg-text-h1 fw-bold">Thank You !</span><br/>
+                        <p class="alg-text-h2 fst-italic">Come back again......</p>
+                    </div>
+                    <button class="alg-bg-dark alg-main-button alg-button-hover p-1 p-lg-2 px-2 px-lg-4 rounded-4 fw-bold alg-text-h2 alg-cursor"><a href="index.php" class="text-decoration-none text-white">Back to Home</a></button>
+                </div>
+            </div>
+        </div>
+    </section>
+</body>
+
+</html>
