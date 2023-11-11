0b63775e44ed6cf61da9f55047ae27b6502b8bac
janith nirmal <113751270+janithnirmal@users.noreply.github.com>
2023-11-12 00:32:49 +0530

    fix

--- backend/model/mail/MailSender.php
+++ backend/model/mail/MailSender.php
@@ -39,7 +39,7 @@ final class MailSender
             $this->mail->Username = $this->senderMail;
             $this->mail->Password = $this->password;
             $this->mail->SMTPSecure = 'tls';
-            $this->mail->Port = 465;
+            $this->mail->Port = 587;
             $this->mail->setFrom($this->senderMail, $title);
             $this->mail->addReplyTo($this->senderMail, $title);
             $this->mail->addAddress($this->toAddress);
