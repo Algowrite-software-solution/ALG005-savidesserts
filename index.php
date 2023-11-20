e1b6daa26b9216881d228c3b28fb6fc92ed8462e
janith nirmal <113751270+janithnirmal@users.noreply.github.com>
2023-11-20 19:01:10 +0530

    updated

--- index.php
+++ index.php
@@ -75,7 +75,7 @@
                 <div class="promotionSwiper swiper mySwiperPromotion mt-2">
                     <div class="swiper-wrapper" id="promotionSliderContainer">
                         <!-- image container goes here -->
-                        
+
                     </div>
                 </div>
             </div>
@@ -171,17 +171,11 @@
                             <div>
                                 <h4 class="mt-3 alg-text-tan">Journey of sawee dessert.....</h4>
                                 <p class="alg-text-light">
-                                    Sawee dessert started at 2014 as a small business and continued till today with succussful 7 years of a journey.
-                                    In the early stages we managed to deliver the products to the customers trough our trusted agest around the country.
-                                    At the moment we have agents on Gampaha, Kandy, Yatiyanthota and Anuradhapura. They managed to deliver the producsts with in the mentioned ares while keeping the freshness
-                                    and goodness.We are capable of delivering dessert products to your hand with in 36 Hours of the order with the well care and quality of the products. It is a
-                                    huge achivement that we have gained troughout this business journey.
+                                    Sawee Dessert started in 2014 as a small business and continues today with a successful 7-year journey. In the early stages, we managed to deliver the products to the customers through our trusted agest around the country. At the moment we have agents in Gampaha, Kandy, Yatiyanthota, and Anuradhapura. They managed to deliver the products within the mentioned areas while keeping the freshness and goodness. We are capable of delivering dessert products to your hand within 36 Hours of the order with the good care and quality of the products. It is a huge achievement that we have gained throughout this business journey.
                                     <br>
                                     <br>
                                     <br>
-                                    Even in the echnomical crisis tha are happening in the country, we still managed to provide you a well prepared desserts with the same quality in a fare
-                                    price range. We hope to continue with this and provide an amazing service to you. Our motive is providing healthy, tasty and clean dessrt to our beloved
-                                    customers who enjoy the tast of sawee dessrt.
+                                    Even in the economic crisis that is happening in the country, we still managed to provide you with a well-prepared dessert of the same quality in a fair price range. We hope to continue with this and provide an amazing service to you. Our motive is to provide healthy, tasty, and clean desserts to our beloved customers who enjoy the taste of sawee dessert.
                                 </p>
                             </div>
                         </div>
@@ -286,7 +280,7 @@
                                     <div class="col-10 col-md-11 col-lg-8">
                                         <div class="bg-white p-2 d-flex align-items-center gap-5 px-3 alg-div-hover rounded-4">
                                             <span><i class='bx bx-mobile fs-5 fw-bold'></i></span>
-                                            <span class="fw-bold alg-text-h3">+94771378492</span>
+                                            <span class="fw-bold alg-text-h3">+94 76 677 3539</span>
                                         </div>
                                     </div>
                                 </div>
