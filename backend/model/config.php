34faa10778cc5ef2a83f3043c674eb4bc123ff75
janith nirmal <113751270+janithnirmal@users.noreply.github.com>
2023-11-24 14:26:08 +0530

    server db config updateds

--- backend/model/config.php
+++ backend/model/config.php
@@ -1,12 +1,6 @@
 <?php
 
-// define("DB_HOST", "localhost");
-// define("DB_USERNAME", "root");
-// define("DATABASE", "savi_dessert_shop");
-// define("DB_PASSWORD", "#Apeamma2001");
-
-
 define("DB_HOST", "localhost");
-define("DB_USERNAME", "root");
-define("DATABASE", "alg005_db");
-define("DB_PASSWORD", "JanithNirmal12#$");
+define("DB_USERNAME", "algowrit_savidessert");
+define("DATABASE", "algowrit_savidessert");
+define("DB_PASSWORD", "CytsTeeY%fw-");
