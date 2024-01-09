0ece0f8f621c3c2d6942e81878d808c548ea3489
janith nirmal <113751270+janithnirmal@users.noreply.github.com>
2024-01-10 03:55:27 +0530

    condig updated

--- backend/model/config.php
+++ backend/model/config.php
@@ -6,12 +6,18 @@
 // define("DB_PASSWORD", "#Apeamma2001");
 
 
-define("DB_HOST", "localhost");
-define("DB_USERNAME", "root");
-define("DATABASE", "alg005_db");
-define("DB_PASSWORD", "JanithNirmal12#$");
+// define("DB_HOST", "localhost");
+// define("DB_USERNAME", "root");
+// define("DATABASE", "alg005_db");
+// define("DB_PASSWORD", "JanithNirmal12#$");
 
 // define("DB_HOST", "localhost");
 // define("DB_USERNAME", "root");
 // define("DATABASE", "alg005_db");
 // define("DB_PASSWORD", "Assiment@1234ABC");
+
+
+define("DB_HOST", "localhost");
+define("DB_USERNAME", "algowrit_savidessert");
+define("DATABASE", "algowrit_savidessert");
+define("DB_PASSWORD", "CytsTeeY%fw-");
